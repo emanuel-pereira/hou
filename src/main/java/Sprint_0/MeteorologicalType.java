c9c0df77bf26955940b24cea2f64aabe5ade88f6
amilcarGomes <1181686@isep.ipp.pt>
2018-11-23 11:55:58 +0000

    Class MeteorologicalType with attribute and constructor

--- src/main/java/Sprint_0/MeteorologicalType.java
+++ src/main/java/Sprint_0/MeteorologicalType.java
@@ -1,4 +1,14 @@
 package Sprint_0;
 
 public class MeteorologicalType {
+
+    //Atributos da classe
+    private String _meteorologicalTypeDesignation;
+
+    //Construtor
+    public MeteorologicalType (String designation){}
+
+
+
+
 }
