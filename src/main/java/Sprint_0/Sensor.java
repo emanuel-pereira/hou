aa24ad1f70f82f9111248519f40ea04587639622
José Ferreira <1181713@isep.ipp.pt>
2018-11-23 11:52:55 +0000

    Creation of attributes and constructor of class Sensor.

--- src/main/java/Sprint_0/Sensor.java
+++ src/main/java/Sprint_0/Sensor.java
@@ -1,5 +1,26 @@
 package Sprint_0;
 
+import java.util.Date;
+import java.util.List;
+
 public class Sensor {
-    private String name;
+    private String _designation;
+    private Date _startDate;
+    private Location _location;
+    private MeteorologicalType _meteorologicalType;
+    private List<Reading> _reading;
+
+    /**
+     * Constructor requiring to set a specific designation, startDate, location and metereologicalType for any object of type Sensor created
+     * @param designation every object of type sensor must have a designation.
+     * @param startDate start date every object of type sensor must have a start date.
+     * @param meteorologicalType every object of type sensor must have a metereological type.
+     * @param location every object of type sensor must have a location.
+     */
+    public Sensor(String designation, Date startDate, Location location, MeteorologicalType meteorologicalType){
+        this._designation=designation;
+        this._startDate=startDate;
+        this._meteorologicalType=meteorologicalType;
+        this._location=location;
+    }
 }
