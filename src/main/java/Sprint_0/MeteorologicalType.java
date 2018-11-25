70e6df2f53b734102b8bda8f6221c884ca3714f8
amilcarGomes <1181686@isep.ipp.pt>
2018-11-25 15:40:09 +0000

    Class MeteorologicalType with set and get methods and tests

--- src/main/java/Sprint_0/MeteorologicalType.java
+++ src/main/java/Sprint_0/MeteorologicalType.java
@@ -6,9 +6,25 @@ public class MeteorologicalType {
     private String _meteorologicalTypeDesignation;
 
     //Construtor
-    public MeteorologicalType (String designation){}
+    public MeteorologicalType(String designation) {
+        setMeteorologicalTypeDesignation (designation);
+    }
 
 
+    //Métodos
+
+    public String getMeteorologicalTypeDesignation() {
+        return this._meteorologicalTypeDesignation;
+    }
+
+
+    public boolean setMeteorologicalTypeDesignation(String designation) {
+        if (designation == null || designation.trim().isEmpty()) {
+            return false;
+        }
+        this._meteorologicalTypeDesignation = designation;
+        return true;
+    }
 
 
 }
