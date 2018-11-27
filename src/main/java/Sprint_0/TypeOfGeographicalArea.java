a24cbca21b142827cc8894ff4cd2eb32345dff0c
Emanuel Pereira <1181704@isep.ipp.pt>
2018-11-27 11:58:43 +0000

    TypeOfGeograficalArea - gettypeArea/settypeArea

--- src/main/java/Sprint_0/TypeOfGeographicalArea.java
+++ src/main/java/Sprint_0/TypeOfGeographicalArea.java
@@ -1,128 +1,46 @@
 package Sprint_0;
 
+
 public class TypeOfGeographicalArea {
 
 
 
-    private String areaName;
+    private String _typeArea;
 
 
+    public TypeOfGeographicalArea (String typeArea) {
 
-    public TypeOfGeographicalArea (){
+        this._typeArea = typeArea;
 
     }
 
-    public String getAreaName() {
+        public String getTypeArea() {
 
-        return this.areaName;
-    }
-
-    public void setAreaName() {
-
-        this.areaName = areaName;
+        return this._typeArea;
     }
 
 
+    public void setTypeArea (String typeArea) {
+
+        this._typeArea = typeArea;
+
+    }
+
+
+    typeArea street = typeArea.street;
+
+    typeArea village = typeArea.village;
+
+    typeArea city = typeArea.city;
+
+    typeArea district = typeArea.district;
+
+
+
+   enum typeArea {
+
+        street, village, city, district
+   }
 
 }
-
-
-    /*
-
-
-        public class Tipo AG {
-
-            private string "nome/designação"
-
-        }
-************************************************
-
-
-        public class localização {
-
-        private
-
-        }
-
-  *********************************************
-
-        public class AG {
-
-
-            private string "nome/designação";
-            private localização;
-            private List<sensor>sensores;
-            private tipoAG tipo;
-            private AG inseridaEm;
-        }
-************************************************
-
-        public class sensor {
-
-            private string "nome";
-
-            private DATE dataFuncionamento;
-
-            private localizacao = localizacao;
-
-            private carateristicasMeteorologicas carateristicas;
-
-            private list<leituras> registos;
-
-        }
-
-*************************************************
-
-
-
-        public class carateristicasMetereologicas {
-
-
-              private "designaçã/nome"
-
-
-              }
-
-******************************************************
-
-
-        public class leiturasSonsores {
-
-
-            private data/hora
-
-            private valor
-
-
-            }
-
-*****************************************************
-
-
-
-        @TESt
-
-
-        public void agInseridaEmDireto()
-
-
-            //arrange
-
-            AG ag1 = new AG("AMP",.......);
-            AG ag2 = new AG ("Porto",.......);
-            AG ag3 = new AG ("Portugal",.....);
-
-            //ACT
-
-            ag2.setInseridaEM(ag1));
-
-            ag2.setINseridaEM(ag1);
-
-
-            //assert
-
-            assertEquals(ag2INseridaEmDireto(ag3),true);
-
-
-*/
 
