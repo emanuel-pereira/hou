12facd6d568a44e38638a94b1fa2d013f60ca942
Emanuel Pereira <1181704@isep.ipp.pt>
2018-11-30 11:05:33 +0000

    TypeOfGeograficalArea - correcoes enumType.

--- src/main/java/Sprint_0/TypeOfGeographicalArea.java
+++ src/main/java/Sprint_0/TypeOfGeographicalArea.java
@@ -4,56 +4,7 @@ package Sprint_0;
 public enum TypeOfGeographicalArea {
 
         STREET, VILLAGE, CITY, DISTRICT, COUNTRY
-
    }
 
-    private String typeArea0;
 
 
-    public TypeOfGeographicalArea (String typeArea) {
-
-        this.typeArea0 = typeArea;
-
-    }
-
-        public String getTypeArea() {
-
-        return this.typeArea0;
-    }
-
-
-    public void setTypeArea (String typeArea) {
-
-        this.typeArea0 = typeArea;
-
-    }
-
-
-    typeArea STREET = typeArea.STREET;
-
-    typeArea village = typeArea.VILLAGE;
-
-    typeArea city = typeArea.CITY;
-
-    typeArea district = typeArea.DISTRICT;
-
-    typeArea country = typeArea.COUNTRY;
-
-
-
-   enum typeArea {
-
-        STREET, VILLAGE, CITY, DISTRICT, COUNTRY
-   }
-
-}
-
-/*
-b. Funcionalidades:
-        - determinar distância linear entre duas áreas geográficas (medido a partir do ponto central);
-        - determinar distância linear entre dois dispositivos/sensores;
-        - determinar os últimos valores da temperatura/pluviosidade/humidade/vento/visibilidade lidas pelos sensores disponíveis na área geográfica;
-        - determinar temperatura/pluviosidade/humidade/vento/visibilidade médias mínima e máxima mensais num dispositivo/sensor;
-
-
-        */
