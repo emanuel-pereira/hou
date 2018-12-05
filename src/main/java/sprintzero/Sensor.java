08dec5e9ee56ddc2d5c4b49342109befbf0c20df
Francisca_1181706 <1181706@isep.ipp.pt>
2018-12-05 16:56:37 +0000

    Fixed Code Smell "The return value of a method call has been replaced without breaking the tests" (L85_Sensor class)

--- src/main/java/sprintzero/Sensor.java
+++ src/main/java/sprintzero/Sensor.java
@@ -61,8 +61,6 @@ public class Sensor {
      * Method to check if the designation given to name the sensor meets the criteria defined to be
      * considered a valid designation, namely:
      * - mDesignation cannot be empty or null
-     * - mDesignation must have only alphabetic characters and a maximum length of 40 characters.
-     *
      * @param designation
      * @return
      */
