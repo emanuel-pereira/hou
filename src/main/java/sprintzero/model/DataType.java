268fbac14d0dd1b83abbd1611bd29b19333d3fe5
Patricia Castro <1181688@isep.ipp.pt>
2018-12-12 16:27:34 +0000

    Ref Issue #13 - User Story 5: create DataType and DataTypeList classes and its methods and tests; correction of SensorTest and GeographicalAreaTest regarding DataType tests

--- src/main/java/sprintzero/model/DataType.java
+++ src/main/java/sprintzero/model/DataType.java
@@ -1,5 +1,69 @@
 package sprintzero.model;
 
-public enum  DataType {
-    TEMPERATURE, PRECIPITATION, HUMIDITY, WIND, VISIBILITY
+
+import org.jetbrains.annotations.Contract;
+
+import java.util.Objects;
+
+public class DataType {
+
+
+    private String mDataType;
+
+
+    /**
+     * Constructor method that defines a designation for a type of data
+     *
+     * @param dataTypeDesignation - String that names the type of data
+     */
+    public DataType(String dataTypeDesignation) {
+        if (this.typeOfDataDesignationIsValid(dataTypeDesignation)) {
+            this.mDataType = dataTypeDesignation;
+        }
+    }
+
+    /**
+     * Method to validate the designation given to a type of data
+     *
+     * @param dataTypeDesignation - String that names the type of data
+     * @return true if the designation is not null or empty after trimming the spaces
+     */
+    public boolean typeOfDataDesignationIsValid(String dataTypeDesignation) {
+        return dataTypeDesignation != null && !dataTypeDesignation.trim().isEmpty();
+    }
+
+
+    /**
+     * Method to return a data type designation
+     *
+     * @return designation of a data type
+     */
+    public String getDataTypeDesignation() {
+        return this.mDataType;
+    }
+
+
+    @Contract(value = "null -> false", pure = true)
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (!(o instanceof DataType)) {
+            return false;
+        }
+        DataType dataType = (DataType) o;
+        if (this.mDataType.equals(dataType.getDataTypeDesignation())) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(mDataType);
+    }
 }
+
+
