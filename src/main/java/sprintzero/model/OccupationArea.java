fafe315bb35b99f5c60d280722631e7d38db9311
filipe0doria <1130778@isep.ipp.pt>
2018-12-12 17:04:37 +0000

    OcupationArea update to re-add occupation area calculation method

--- src/main/java/sprintzero/model/OccupationArea.java
+++ src/main/java/sprintzero/model/OccupationArea.java
@@ -2,7 +2,7 @@ package sprintzero.model;
 
 public class OccupationArea {
 
-
+    private double mOccupation = Double.NaN;
     private double mHeight;
     private double mWidth;
 
@@ -10,18 +10,25 @@ public class OccupationArea {
     /**
      * Constructor method that determines a length and width for an occupational area and it verifies if
      * both attributes have positive values
+     *
      * @param inputHeight - height of the occupation area
      * @param inputWidth  - width of the occupation area
-     * @return true if both expressions are positive or false if one of them is equal or less than zero
      */
     public OccupationArea(double inputHeight, double inputWidth) {
-        this.mHeight=inputHeight;
-        this.mWidth= inputWidth;
+        this.mHeight = inputHeight;
+        this.mWidth = inputWidth;
+        if (inputWidth > 0 && inputHeight > 0) {
+            this.mOccupation = inputHeight * inputWidth;
+        }
     }
+
+    //to add inputs verification
+    //true if both expressions are positive or false if one of them is equal or less than zero
 
     /**
      * Method to get height of an occupation area
-     * @return
+     *
+     * @return GA Height
      */
     public double getmHeight() {
         return mHeight;
@@ -29,9 +36,18 @@ public class OccupationArea {
 
     /**
      * Method to get width of an occupation area
-     * @return
+     *
+     * @return GA Width
      */
     public double getmWidth() {
         return mWidth;
+    }
+
+    /**
+     * Method that returns the area occupied by a geographical area
+     * @return - value of occupation area
+     */
+    public double getOccupationArea() {
+        return this.mOccupation;
     }
 }
