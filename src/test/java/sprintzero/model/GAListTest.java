6d5d697514ca864cb60b76ec14aff06da744bf5f
José Ferreira <1181713@isep.ipp.pt>
2018-12-12 16:48:58 +0000

    ref #14 Creation of methods to check if a location is within a geographical area in a list of geographical areas + tests. This method will be used to check which geographical area contains the location of a sensor created and then assign the sensor to the list of sensors of that geographical area.

--- src/test/java/sprintzero/model/GAListTest.java
+++ src/test/java/sprintzero/model/GAListTest.java
@@ -1,6 +1,10 @@
 package sprintzero.model;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -15,6 +19,30 @@ class GAListTest {
     }
 
     @Test
-    void getGAList() {
+    @DisplayName("Test if checkIfLocationIsInGAList method returns ga2")
+    void checkIfLocationIsInGAList() {
+        GAList gaList= new GAList();
+       GeographicalArea ga1= new GeographicalArea("Gaia","City",20,20,2,2,5);
+        gaList.addGA(ga1);
+        GeographicalArea ga2= new GeographicalArea("Matosinhos","City",4,5,2,2,5);
+        gaList.addGA(ga2);
+        Location loc = new Location(2.7,5.2,0);
+        GeographicalArea expectedResult=ga2;
+        GeographicalArea result=gaList.checkIfLocationIsInGAList(loc.getLatitude(),loc.getLongitude());
+        assertEquals(expectedResult,result);
+    }
+
+    @Test
+    @DisplayName("Ensure that Location loc is not within any geographical area in gaList")
+    void checkIfLocationIsNotInGAList() {
+        GAList gaList= new GAList();
+        GeographicalArea ga1= new GeographicalArea("Gaia","City",20,20,2,2,5);
+        gaList.addGA(ga1);
+        GeographicalArea ga2= new GeographicalArea("Matosinhos","City",4,5,2,2,5);
+        gaList.addGA(ga2);
+        Location loc = new Location(22.7,5.2,0);
+        GeographicalArea expectedResult=null;
+        GeographicalArea result=gaList.checkIfLocationIsInGAList(loc.getLatitude(),loc.getLongitude());
+        assertEquals(expectedResult,result);
     }
 }
