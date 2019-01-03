3e77373578b4c399e1aff974219e33f150177733
Filipe Dória <1130778@isep.ipp.pt>
2019-01-03 20:21:21 +0000

    US108 import update to fix build

--- src/test/java/smarthome/controller/US108EditRoomOfHouseCTRLTest.java
+++ src/test/java/smarthome/controller/US108EditRoomOfHouseCTRLTest.java
@@ -1,7 +1,7 @@
 package smarthome.controller;
 
-import org.junit.Test;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import smarthome.model.Room;
 
 import java.util.ArrayList;
