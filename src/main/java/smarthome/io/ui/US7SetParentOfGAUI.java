947c2bf2f242797aaf2bc771dc7dcbbf6e6fe3ff
Gonçalo Pereira <1181708@isep.ipp.pt>
2019-01-04 12:02:36 +0000

    Minor update to US7UI

--- src/main/java/smarthome/io/ui/US7SetParentOfGAUI.java
+++ src/main/java/smarthome/io/ui/US7SetParentOfGAUI.java
@@ -4,6 +4,7 @@ import smarthome.controller.US7SetParentOfGACTRL;
 import smarthome.model.GAList;
 
 import java.util.Scanner;
+
 public class US7SetParentOfGAUI {
 
     private GAList mGAList;
@@ -17,16 +18,27 @@ public class US7SetParentOfGAUI {
     public void run() {
         Scanner read = new Scanner(System.in);
 
-        System.out.println("Choose a Geographical Area from the list below: ");
-        System.out.println(mCtrlUS7.showListInString());
-        int gaIndex1 = read.nextInt();
-        System.out.println("Success");
-
-        System.out.println("Choose a Geographical Area from the list below to set as parent of " + mGAList.getGAList().get(gaIndex1 - 1).getGeographicalAreaDesignation());
-        System.out.println(mCtrlUS7.showListInString());
-        int gaIndex2 = read.nextInt();
-        System.out.println("Success");
-
-        mCtrlUS7.setParentofGA(gaIndex1, gaIndex2);
+        while (true) {
+            System.out.println("Choose a Geographical Area from the list below. (or insert 0 to return to Main Menu): ");
+            System.out.println(mCtrlUS7.showListInString());
+            int gaIndex1 = read.nextInt();
+            if (gaIndex1 == 0) {
+                System.out.println("Return to Main Menu");
+                break;
+            }
+            System.out.println("Success");
+            int gaIndex2;
+            while (true) {
+                System.out.println("Choose a Geographical Area from the list below to set as parent of " + mGAList.getGAList().get(gaIndex1 - 1).getGeographicalAreaDesignation());
+                System.out.println(mCtrlUS7.showListInString());
+                gaIndex2 = read.nextInt();
+                if (gaIndex1 == gaIndex2) {
+                    System.out.println("Error. Please choose a different area");
+                } else break;
+            }
+            mCtrlUS7.setParentofGA(gaIndex1, gaIndex2);
+            System.out.println("Success");
+            break;
+        }
     }
 }
