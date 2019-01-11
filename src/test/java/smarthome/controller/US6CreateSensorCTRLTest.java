879621b4db8635ae88be96b6b2f7d308117d7615
amilcarGomes <1181686@isep.ipp.pt>
2019-01-11 10:24:26 +0000

    Create ReadingList Fix#46

--- src/test/java/smarthome/controller/US6CreateSensorCTRLTest.java
+++ src/test/java/smarthome/controller/US6CreateSensorCTRLTest.java
@@ -66,15 +66,15 @@ class US6CreateSensorCTRLTest {
 
         Reading r1Porto = new Reading(15, new GregorianCalendar(2018, 12, 26, 12, 00));
         Reading r2Porto = new Reading(18, new GregorianCalendar(2018, 12, 26, 13, 00));
-        List<Reading> readingsPt = new ArrayList<>();
-        readingsPt.add(r1Porto);
-        readingsPt.add(r2Porto);
+        ReadingList readingsPt = new ReadingList();
+        readingsPt.addReading (r1Porto);
+        readingsPt.addReading (r2Porto);
 
         Reading r1Lis = new Reading(27, new GregorianCalendar(2018, 12, 26, 12, 00));
         Reading r2Lis = new Reading(21, new GregorianCalendar(2018, 12, 26, 13, 00));
-        List<Reading> readingsLis = new ArrayList<>();
-        readingsLis.add(r1Lis);
-        readingsLis.add(r2Lis);
+        ReadingList readingsLis = new ReadingList();
+        readingsLis.addReading (r1Lis);
+        readingsLis.addReading (r2Lis);
 
         boolean result = ctrl6.addNewSensorToGA("LisboaTempSensor", new GregorianCalendar(2018, 12, 26), 1, "C", 55, 40, 15, 1, readingsPt);
         assertTrue(result);
@@ -88,7 +88,7 @@ class US6CreateSensorCTRLTest {
     public void throwsIllegalArgumentExceptionForLatitude() {
         SensorTypeList sensorTypeList = new SensorTypeList();
         GAList gaList = new GAList();
-        List<Reading> readingsPt = new ArrayList<>();
+        ReadingList readingsPt = new ReadingList ();
 
         GeographicalArea ga1 = new GeographicalArea("Pt", "Porto", "city", 25, 15, 12, 32, 41);
         gaList.addGA(ga1);
@@ -97,7 +97,7 @@ class US6CreateSensorCTRLTest {
         sensorTypeList.addSensorType(type1);
 
         Reading r1Porto = new Reading(15, new GregorianCalendar(2018, 12, 26, 12, 00));
-        readingsPt.add(r1Porto);
+        readingsPt.addReading (r1Porto);
 
         US6CreateSensorCTRL ctrl6 = new US6CreateSensorCTRL(sensorTypeList,gaList);
 
@@ -118,7 +118,7 @@ class US6CreateSensorCTRLTest {
     public void throwsIllegalArgumentExceptionForLongitude() {
         SensorTypeList sensorTypeList = new SensorTypeList();
         GAList gaList = new GAList();
-        List<Reading> readingsPt = new ArrayList<>();
+        ReadingList readingsPt = new ReadingList();
 
         GeographicalArea ga1 = new GeographicalArea("Pt", "Porto", "city", 25, 15, 12, 32, 41);
         gaList.addGA(ga1);
@@ -127,7 +127,7 @@ class US6CreateSensorCTRLTest {
         sensorTypeList.addSensorType(type1);
 
         Reading r1Porto = new Reading(15, new GregorianCalendar(2018, 12, 26, 12, 00));
-        readingsPt.add(r1Porto);
+        readingsPt.addReading (r1Porto);
 
         US6CreateSensorCTRL ctrl6 = new US6CreateSensorCTRL(sensorTypeList,gaList);
 
@@ -148,7 +148,7 @@ class US6CreateSensorCTRLTest {
     public void throwsIllegalArgumentExceptionForAltitude() {
         SensorTypeList sensorTypeList = new SensorTypeList();
         GAList gaList = new GAList();
-        List<Reading> readingsPt = new ArrayList<>();
+        ReadingList readingsPt = new ReadingList ();
 
         GeographicalArea ga1 = new GeographicalArea("Pt", "Porto", "city", 25, 15, 12, 32, 41);
         gaList.addGA(ga1);
@@ -157,7 +157,7 @@ class US6CreateSensorCTRLTest {
         sensorTypeList.addSensorType(type1);
 
         Reading r1Porto = new Reading(15, new GregorianCalendar(2018, 12, 26, 12, 00));
-        readingsPt.add(r1Porto);
+        readingsPt.addReading (r1Porto);
 
         US6CreateSensorCTRL ctrl6 = new US6CreateSensorCTRL(sensorTypeList,gaList);
 
