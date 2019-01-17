27300ba6e22a126b462b0a677334a93090cdd16c
amilcarGomes <1181686@isep.ipp.pt>
2019-01-17 16:11:19 +0000

    US215 Edit Sequence Diagram Date Validations: Define a constant instead of duplicating

--- src/main/java/smarthome/model/DateValidations.java
+++ src/main/java/smarthome/model/DateValidations.java
@@ -4,16 +4,17 @@ public class DateValidations {
 
     /**
      * Method that checks if year inputted by the user is valid considering a range between 2010 and 2099
+     *
      * @param year input year
      * @return year if input meets regex criteria, otherwise returns null
      */
     public String yearIsValid(String year) {
-        if (year.trim().isEmpty()) {
-            System.out.println("Empty spaces are not accepted");
+        if (year.trim ().isEmpty ()) {
+            this.emptySpacesMessage ();
             return null;
         }
-        if (!year.matches("^201[0-9]|20[2-9][0-9]$")) { //only accepts years between 2010 and 2099
-            System.out.println("Please insert a valid year between 2010 and 2099.");
+        if (!year.matches ("^201[0-9]|20[2-9][0-9]$")) { //only accepts years between 2010 and 2099
+            System.out.println ("Please insert a valid year between 2010 and 2099.");
             return null;
         }
         return year;
@@ -21,16 +22,17 @@ public class DateValidations {
 
     /**
      * Method that checks if month inputted by the user is valid considering a range regex values between 1 to 12
+     *
      * @param inputMonth input month
      * @return month if input meets regex criteria, otherwise returns null
      */
     public String monthIsValid(String inputMonth) {
-        if (inputMonth.trim().isEmpty()) {
-            System.out.println("Empty spaces are not accepted");
+        if (inputMonth.trim ().isEmpty ()) {
+            this.emptySpacesMessage ();
             return null;
         }
-        if (!inputMonth.matches("^([1-9]|1[0-2])$")) { //only accepts values between 1 and 12
-            System.out.println("Please insert a valid month.");
+        if (!inputMonth.matches ("^([1-9]|1[0-2])$")) { //only accepts values between 1 and 12
+            System.out.println ("Please insert a valid month.");
             return null;
         }
         return inputMonth;
@@ -39,34 +41,35 @@ public class DateValidations {
     /**
      * Method that checks if day inputted by the user is a valid day considering previously inputted month and year
      * considering leap years.
-     * @param day input day
+     *
+     * @param day        input day
      * @param inputMonth previously inputted by the user parsed from string to integer
-     * @param inputYear previously inputted by the user parsed from string to integer
+     * @param inputYear  previously inputted by the user parsed from string to integer
      * @return day if input meets regex criteria, otherwise returns null
      */
     public String dayIsValid(String day, int inputMonth, int inputYear) {
-        if (day.trim().isEmpty()) {
-            System.out.println("Empty spaces are not accepted");
+        if (day.trim ().isEmpty ()) {
+            this.emptySpacesMessage ();
             return null;
         }
-        if (!day.matches("^(3[01]|[12][0-9]|[1-9])$")) { //only accepts values between 1 and 31
-            System.out.println("Please insert a valid day.");
+        if (!day.matches ("^(3[01]|[12][0-9]|[1-9])$")) { //only accepts values between 1 and 31
+            System.out.println ("Please insert a valid day.");
             return null;
         }
-        if (inputMonth==4||inputMonth==6||inputMonth==9||inputMonth==11) {
-            if (day.matches("^31$")) {
-                System.out.println("Please insert a valid day for the selected month: (" + inputMonth + ")");
+        if (inputMonth == 4 || inputMonth == 6 || inputMonth == 9 || inputMonth == 11) {
+            if (day.matches ("^31$")) {
+                System.out.println ("Please insert a valid day for the selected month: (" + inputMonth + ")");
                 return null;
             }
         }
-        if (inputMonth==2) {
+        if (inputMonth == 2) {
             if (inputYear % 4 != 0) {
-                if (day.matches("^(29|30|31)$")) {
-                    System.out.println("Please insert a valid day for the selected month: (" + inputMonth + ")");
+                if (day.matches ("^(29|30|31)$")) {
+                    System.out.println ("Please insert a valid day for the selected month: (" + inputMonth + ")");
                     return null;
                 }
-            } else if (day.matches("^(30|31)$")) {
-                System.out.println("Please insert a valid day for the selected month: (" + inputMonth + ")");
+            } else if (day.matches ("^(30|31)$")) {
+                System.out.println ("Please insert a valid day for the selected month: (" + inputMonth + ")");
                 return null;
             }
         }
@@ -75,18 +78,27 @@ public class DateValidations {
 
     /**
      * Method that checks if hour inputted by the user is a valid
+     *
      * @param hour input hour
      * @return hour if input meets regex criteria, otherwise returns null
      */
-        public String hourIsValid(String hour){
-            if (hour.trim().isEmpty()) {
-                System.out.println("Empty spaces are not accepted");
-                return null;
-            }
-            if (!hour.matches("^(2[0-4]|[1][0-9]|[1-9])")) {
-                System.out.println("Please insert a valid hour.");
-                return null;
-            }
-            return hour;
+    public String hourIsValid(String hour) {
+        if (hour.trim ().isEmpty ()) {
+            this.emptySpacesMessage ();
+            return null;
         }
+        if (!hour.matches ("^(2[0-4]|[1][0-9]|[1-9])")) {
+            System.out.println ("Please insert a valid hour.");
+            return null;
+        }
+        return hour;
     }
+
+    private void emptySpacesMessage() {
+        System.out.print ("Empty spaces are not accepted.");
+    }
+
+
+
+
+}
