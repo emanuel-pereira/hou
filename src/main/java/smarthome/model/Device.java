8e1c531f2f75c2a2c8964afedb92c7642b2d5473
amilcarGomes <1181686@isep.ipp.pt>
2019-01-18 14:18:23 +0000

    Try to fix bug.

--- src/main/java/smarthome/model/Device.java
+++ src/main/java/smarthome/model/Device.java
@@ -73,33 +73,35 @@ public class Device implements DeviceSpecs, Powered {
     }
 
     public void setRoom(Room newRoom) {
-        mRoom.getDeviceList().getDeviceList().remove(this);
+        mRoom.getDeviceList ().getDeviceList ().remove (this);
         mRoom = newRoom;
-        mRoom.getDeviceList().addDevice(this);
+        mRoom.getDeviceList ().addDevice (this);
     }
 
     public void setNominalPower(double nominalPower) {
         this.mNominalPower = nominalPower;
     }
 
-    public String showDeviceAttributesInString(){
-       StringBuilder result = new StringBuilder();
-        for(String s : getDeviceAttributesInString()){
-           result.append(s);
-           result.append("\n");}
-        return result.toString();}
+    public String showDeviceAttributesInString() {
+        StringBuilder result = new StringBuilder ();
+        for (String s : getDeviceAttributesInString ()) {
+            result.append (s);
+            result.append ("\n");
+        }
+        return result.toString ();
+    }
 
     public List<String> getDeviceAttributesInString() {
-        List<String> result = new ArrayList<>();
-        String deviceName = "1 - Device Name : " + this.getName();
-        String deviceRoom = "2 - Device Room : " + this.mRoom.getName();
+        List<String> result = new ArrayList<> ();
+        String deviceName = "1 - Device Name : " + this.getName ();
+        String deviceRoom = "2 - Device Room : " + this.mRoom.getName ();
         String deviceNominalPower = "3 - Device Nominal Power : " + this.mNominalPower;
-        result.add(deviceName);
-        result.add(deviceRoom);
-        result.add(deviceNominalPower);
+        result.add (deviceName);
+        result.add (deviceRoom);
+        result.add (deviceNominalPower);
         if (this.mDeviceSpecs != null)
-            for (String spec : this.getDeviceSpecs().getDeviceAttributesInString())
-                result.add(spec);
+            for (String spec : this.getDeviceSpecs ().getDeviceAttributesInString ())
+                result.add (spec);
         return result;
     }
 
@@ -107,12 +109,14 @@ public class Device implements DeviceSpecs, Powered {
         String deviceName = "1 - Device Name : " + this.mName;
         String deviceRoom = "2 - Device Room : " + this.mRoom;
         String deviceNominalPower = "3 - Device Nominal Power : " + this.mNominalPower;
-        if (attribute.equals(deviceName))
-            setDeviceName(newValue);
-        if (attribute.equals(deviceNominalPower))
-        setNominalPower(parseDouble(newValue));
-        if (this.mDeviceSpecs != null){
-            this.getDeviceSpecs().setAttributeValue(attribute,newValue);
+        if (attribute.equals (deviceName)) {
+            setDeviceName (newValue);
+        }
+        if (attribute.equals (deviceNominalPower)) {
+            setNominalPower (parseDouble (newValue));
+        }
+        if (this.mDeviceSpecs != null) {
+            this.getDeviceSpecs ().setAttributeValue (attribute, newValue);
         }
     }
 }
