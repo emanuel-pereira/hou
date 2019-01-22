25a9ffc2bff75536b78d01730f0d94ac0267c0a4
amilcarGomes <1181686@isep.ipp.pt>
2019-01-22 00:49:10 +0000

    Rearrange HouseGrid Addresses#62. Add getRoomListInAGridSize to HouseGrid.

--- src/main/java/smarthome/model/HouseGrid.java
+++ src/main/java/smarthome/model/HouseGrid.java
@@ -1,7 +1,6 @@
 package smarthome.model;
 
 import java.util.Objects;
-import smarthome.model.PowerSource;
 
 public class HouseGrid {
     //The electrical network of the house
@@ -42,20 +41,20 @@ public class HouseGrid {
 
     /**
      * Attach a room to a house grid if the room doesn't exists in the grid room list
+     *
      * @param inputRoom Room that will be attached to a grid
      * @return true if the room is added
      */
-    public boolean attachRoomToGrid(Room inputRoom){
-        if (mRoomList.getRoomList ().contains (inputRoom)){
+    public boolean attachRoomToGrid(Room inputRoom) {
+        if (mRoomList.getRoomList ().contains (inputRoom)) {
             return false;
-        }
-        else mRoomList.addRoom (inputRoom);
+        } else mRoomList.addRoom (inputRoom);
         return true;
     }
 
     public boolean detachRoomFromGrid(Room inputRoom) {
         if (mRoomList.getRoomList ().contains (inputRoom)) {
-            mRoomList.getRoomList().remove (inputRoom);
+            mRoomList.getRoomList ().remove (inputRoom);
             return true;
         } else return false;
     }
@@ -63,24 +62,31 @@ public class HouseGrid {
 
     /**
      * Get a copy of the list of rooms in a grid
+     *
      * @return list of rooms in a grid
      */
     public RoomList getRoomListInAGrid() {
-         RoomList roomList = new RoomList ();
-            for (Room room : mRoomList.getRoomList ()) {
-                roomList.addRoom (room);
-            }
-            return roomList;
+        RoomList roomList = new RoomList ();
+        for (Room room : mRoomList.getRoomList ()) {
+            roomList.addRoom (room);
         }
+        return roomList;
+    }
+
+    public int getRoomListInAGridSize() {
+        return mRoomList.getRoomListSize ();
+    }
+
 
     /**
      * Nominal power of a grid is the sum of nominal power of all the rooms in tat grid
+     *
      * @return nominal power of a grid
      */
     public double getNominalPower() {
         double sum = 0;
         for (Room room : getRoomListInAGrid ().getRoomList ()) {
-                sum += room.getNominalPower ();
+            sum += room.getNominalPower ();
         }
         return sum;
     }
