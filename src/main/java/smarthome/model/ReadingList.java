00588bc4618c552ede01b9c3b1d3a5851fa3f913
filipe0doria <1130778@isep.ipp.pt>
2019-02-01 00:52:59 +0000

    Build correction and new method getReadingsInTimeInterval;

--- src/main/java/smarthome/model/ReadingList.java
+++ src/main/java/smarthome/model/ReadingList.java
@@ -9,12 +9,12 @@ public class ReadingList {
 
     private List<Reading> mReadingList;
 
-    public ReadingList(){
-        mReadingList = new ArrayList<> ();
+    public ReadingList() {
+        mReadingList = new ArrayList<>();
     }
 
-    public Reading newReading (double readValue, Calendar timeOfReading) {
-        return new Reading (readValue,timeOfReading);
+    public Reading newReading(double readValue, Calendar timeOfReading) {
+        return new Reading(readValue, timeOfReading);
     }
 
     public boolean addReading(Reading newReading) {
@@ -23,7 +23,7 @@ public class ReadingList {
         return mReadingList.add(newReading);
     }
 
-    public List<Reading> getReadingList(){
+    public List<Reading> getReadingList() {
         return mReadingList;
     }
 
@@ -33,31 +33,39 @@ public class ReadingList {
      * @param inputDate
      * @return totalRainfallValue
      */
-
     public double totalValueInGivenDay(GregorianCalendar inputDate) {
         GregorianCalendar date = inputDate;
         double totalRainfallValue = 0;
 
-        for (Reading reading: mReadingList) {
-            if(reading.getDateAndTime().get(Calendar.DATE) == inputDate.get(Calendar.DATE)) {
+        for (Reading reading : mReadingList) {
+            if (reading.getDateAndTime().get(Calendar.DATE) == inputDate.get(Calendar.DATE)) {
                 totalRainfallValue = reading.returnValueOfReading() + totalRainfallValue;
             }
         }
-
-
         return totalRainfallValue;
     }
 
-    public double getValueOfReadingsInTimeInterval(Calendar startDate, Calendar endDate){
-        double totalValue=0;
-        for(Reading reading:mReadingList){
-            Calendar readingTime=reading.getDateAndTime();
-            if(readingTime.after(startDate)&&readingTime.before(endDate)||readingTime.equals(endDate)){
-                totalValue+=reading.returnValueOfReading();
+    public double getValueOfReadingsInTimeInterval(Calendar startDate, Calendar endDate) {
+        double totalValue = 0;
+        for (Reading reading : mReadingList) {
+            Calendar readingTime = reading.getDateAndTime();
+            if (readingTime.after(startDate) && readingTime.before(endDate) || readingTime.equals(endDate)) {
+                totalValue += reading.returnValueOfReading();
             }
         }
         return totalValue;
 
     }
 
+    public ReadingList getReadingsInTimeInterval(Calendar startDate, Calendar endDate) {
+        ReadingList readingList = new ReadingList();
+        for (Reading reading : mReadingList) {
+            Calendar readingTime = reading.getDateAndTime();
+
+            if (readingTime.after(startDate) && readingTime.before(endDate) || readingTime.equals(endDate)) {
+                readingList.addReading(reading);
+            }
+        }
+        return readingList;
+    }
 }
