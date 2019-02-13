bd286a382c642af193ebbdc69351a31332641989
amilcarGomes <1181686@isep.ipp.pt>
2019-02-13 16:56:13 +0000

    Define a constant instead of duplicating "Washing Machine Capacity" 3 times.

--- src/main/java/smarthome/model/WashingMachine.java
+++ src/main/java/smarthome/model/WashingMachine.java
@@ -9,6 +9,9 @@ public class WashingMachine implements DeviceSpecs {
     private int mCapacity;
     private DeviceType mDeviceType;
 
+    private String capacity = "Washing Machine Capacity";
+
+
     public WashingMachine() {
     }
 
@@ -26,13 +29,11 @@ public class WashingMachine implements DeviceSpecs {
 
     public List<String> getAttributesNames() {
         List<String> result = new ArrayList<>();
-        String capacity = "Washing Machine Capacity";
         result.add(capacity);
         return result;
     }
 
     public void setAttributeValue(String attribute, String newValue) {
-        String capacity = "Washing Machine Capacity";
         if (attribute.contains(capacity))
             setCapacity(parseInt(newValue));
 
@@ -44,7 +45,7 @@ public class WashingMachine implements DeviceSpecs {
         for (String s : getAttributesNames()) {
             result.append(number);
             result.append(" - ");
-            if (s.contains("Washing Machine Capacity"))
+            if (s.contains(capacity))
                 result.append(s.concat(" : " + this.getCapacity()));
             result.append("\n");
             number++;
