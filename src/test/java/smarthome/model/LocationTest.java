5938aa1d43e01da2bd6162bc950606256db3183f
amilcarGomes <1181686@isep.ipp.pt>
2019-02-14 16:04:50 +0000

    Add new test: valid GPS coordinates in GPSValidations class

--- src/test/java/smarthome/model/LocationTest.java
+++ src/test/java/smarthome/model/LocationTest.java
@@ -3,9 +3,7 @@ package smarthome.model;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
 
 class LocationTest {
@@ -14,35 +12,58 @@ class LocationTest {
     @DisplayName("Check if all GPS coordinates are valid while creating an instance of a location.")
     public void checkIfGPSCoordinatesAreValid() {
         //Arrange
-        Location l = new Location(12, -4, 100);
+        Location l = new Location (12, -4, 100);
 
         //Act
         double expectedLat = 12;
-        double resultLat = l.getLatitude();
+        double resultLat = l.getLatitude ();
 
         //Assert
-        assertEquals(expectedLat, resultLat);
+        assertEquals (expectedLat, resultLat);
 
         double expectedLon = -4;
-        double resultLon = l.getLongitude();
-        assertEquals(expectedLon, resultLon);
+        double resultLon = l.getLongitude ();
+        assertEquals (expectedLon, resultLon);
 
         double expectedAlt = 100;
-        double resultAlt = l.getAltitude();
-        assertEquals(expectedAlt, resultAlt);
+        double resultAlt = l.getAltitude ();
+        assertEquals (expectedAlt, resultAlt);
     }
 
     @Test
-    @DisplayName("Check if all GPS coordinates are invalid while creating an instance of a location.")
-    public void checkIfGPSCoordinatesAreInvalid() {
+    public void checkIfLatitudeIsInvalid() {
 
         boolean thrown = false;
         try {
-            new Location(100, 200, 9000);
+            new Location (100, 180, 8848);
         } catch (IllegalArgumentException e) {
             thrown = true;
         }
-        assertTrue(thrown);
+        assertTrue (thrown);
+    }
+
+    @Test
+    public void checkIfLongitudeIsInvalid() {
+
+        boolean thrown = false;
+        try {
+            new Location (90, 200, 8848);
+        } catch (IllegalArgumentException e) {
+            thrown = true;
+        }
+        assertTrue (thrown);
+    }
+
+    @Test
+    public void checkIfAltitudeIsInvalid() {
+
+        boolean thrown = false;
+        try {
+            new Location (90, 180, 9000);
+        } catch (IllegalArgumentException e) {
+            thrown = true;
+        }
+        assertTrue (thrown);
     }
 
 
@@ -50,44 +71,44 @@ class LocationTest {
     @DisplayName("Check that all max limit GPS coordinates are valid while creating an instance of a location.")
     public void ensureAllMaxLimitGPSCoordinatesAreValid() {
         //Arrange
-        Location l = new Location(90, 180, 8848);
+        Location l = new Location (90, 180, 8848);
 
         //Act
         double expectedLat = 90;
-        double resultLat = l.getLatitude();
+        double resultLat = l.getLatitude ();
 
         //Assert
-        assertEquals(expectedLat, resultLat);
+        assertEquals (expectedLat, resultLat);
 
         double expectedLon = 180;
-        double resultLon = l.getLongitude();
-        assertEquals(expectedLon, resultLon);
+        double resultLon = l.getLongitude ();
+        assertEquals (expectedLon, resultLon);
 
         double expectedAlt = 8848;
-        double resultAlt = l.getAltitude();
-        assertEquals(expectedAlt, resultAlt);
+        double resultAlt = l.getAltitude ();
+        assertEquals (expectedAlt, resultAlt);
     }
 
     @Test
     @DisplayName("Check that all min limit GPS coordinates are valid while creating an instance of a location.")
     public void ensureAllMinLimitGPSCoordinatesAreValid() {
         //Arrange
-        Location l = new Location(-90, -180, -12500);
+        Location l = new Location (-90, -180, -12500);
 
         //Act
         double expectedLat = -90;
-        double resultLat = l.getLatitude();
+        double resultLat = l.getLatitude ();
 
         //Assert
-        assertEquals(expectedLat, resultLat);
+        assertEquals (expectedLat, resultLat);
 
         double expectedLon = -180;
-        double resultLon = l.getLongitude();
-        assertEquals(expectedLon, resultLon);
+        double resultLon = l.getLongitude ();
+        assertEquals (expectedLon, resultLon);
 
         double expectedAlt = -12500;
-        double resultAlt = l.getAltitude();
-        assertEquals(expectedAlt, resultAlt);
+        double resultAlt = l.getAltitude ();
+        assertEquals (expectedAlt, resultAlt);
     }
 
     @Test
@@ -100,7 +121,7 @@ class LocationTest {
         } catch (IllegalArgumentException e) {
             thrown = true;
         }
-        assertTrue(thrown);
+        assertTrue (thrown);
     }
 
     @Test
@@ -113,7 +134,7 @@ class LocationTest {
         } catch (IllegalArgumentException e) {
             thrown = true;
         }
-        assertTrue(thrown);
+        assertTrue (thrown);
     }
 
     @Test
@@ -126,7 +147,7 @@ class LocationTest {
         } catch (IllegalArgumentException e) {
             thrown = true;
         }
-        assertTrue(thrown);
+        assertTrue (thrown);
     }
 
     @Test
@@ -139,7 +160,7 @@ class LocationTest {
         } catch (IllegalArgumentException e) {
             thrown = true;
         }
-        assertTrue(thrown);
+        assertTrue (thrown);
     }
 
     @Test
@@ -152,7 +173,7 @@ class LocationTest {
         } catch (IllegalArgumentException e) {
             thrown = true;
         }
-        assertTrue(thrown);
+        assertTrue (thrown);
     }
 
     @Test
@@ -165,32 +186,50 @@ class LocationTest {
         } catch (IllegalArgumentException e) {
             thrown = true;
         }
-        assertTrue(thrown);
+        assertTrue (thrown);
+    }
+
+    @Test
+    void validLatitude() {
+        GPSValidations validations = new GPSValidations ();
+        assertTrue (validations.latitudeIsValid (80));
+    }
+
+    @Test
+    void validLongitude() {
+        GPSValidations validations = new GPSValidations ();
+        assertTrue (validations.longitudeIsValid (100));
+    }
+
+    @Test
+    void validAltitude() {
+        GPSValidations validations = new GPSValidations ();
+        assertTrue (validations.altitudeIsValid (30));
     }
 
     @Test
     public void calcLinearDistanceBetweenL1AndL2Test() {
-        Location l1 = new Location(41.15, -8.6, 83);
-        Location l2 = new Location(38.7, -9.1, 4);
+        Location l1 = new Location (41.15, -8.6, 83);
+        Location l2 = new Location (38.7, -9.1, 4);
         //Arrange
         double result;
         double expectedresult = 79.0395;
         //Act
-        result =l1.calcLinearDistanceBetweenTwoPoints(l1, l2);
+        result = l1.calcLinearDistanceBetweenTwoPoints (l1, l2);
         //Assert
-        assertEquals(expectedresult, result, 0.0001);
+        assertEquals (expectedresult, result, 0.0001);
     }
 
     @Test
     public void calcLinearDistanceBetweenL1AndL2TestNotZero() {
-        Location l1 = new Location(41.15, -8.6, 83);
-        Location l2 = new Location(38.7, -9.1, 4);
+        Location l1 = new Location (41.15, -8.6, 83);
+        Location l2 = new Location (38.7, -9.1, 4);
         //Arrange
         double result;
         double expectedresult = 0;
         //Act
-        result = l1.calcLinearDistanceBetweenTwoPoints(l1, l2);
+        result = l1.calcLinearDistanceBetweenTwoPoints (l1, l2);
         //Assert
-        assertNotEquals(expectedresult, result);
+        assertNotEquals (expectedresult, result);
     }
 }
