bb62b5f8050a927d7ea3ec541aaa8d0ea1914b93
filipe0doria <1130778@isep.ipp.pt>
2019-02-20 16:57:44 +0000

    Power User menu UI reorder to organise options by theme.

--- src/main/java/smarthome/io/ui/PowerUserUI.java
+++ src/main/java/smarthome/io/ui/PowerUserUI.java
@@ -16,13 +16,13 @@ public final class PowerUserUI {
             ArrayList<String> options = new ArrayList<>();
 
             options.add("[1] Show all the devices connected to a grid");
-            options.add("[2] Get the total nominal power connected to a grid");
+            options.add("[2] Deactivate a device");
             options.add("[3] Get the total nominal power of a room");
-            options.add("[4] Estimate the total energy used in heating water in a given day");
-            options.add("[5] Get the total nominal power in a subset of rooms and/or devices");
-            options.add("[6] Deactivate a device");
-            options.add("[7] Get the total energy consumption of a device, a room or a grid in a given time interval");
-            options.add("[8] Get the data series of the metered energy consumption of a device/room/grid in a given time interval");
+            options.add("[4] Get the total nominal power of a grid");
+            options.add("[5] Get the total nominal power of a subset of rooms and/or devices");
+            options.add("[6] Get the total energy consumption of a device, a room or a grid in a given time interval");
+            options.add("[7] Get the total energy consumption of as a data series of a device/room/grid in a given time interval");
+            options.add("[8] Estimate the total energy used in heating water in a given day");
             options.add("[0] Exit");
 
             UtilsUI.showList("Power Usage", options, false, 5);
@@ -33,30 +33,32 @@ public final class PowerUserUI {
                     UtilsUI.underMaintenanceMsg("US160");
                     break;
                 case 2:
-                    GetGridTotalNominalPowerUI uS172 = new GetGridTotalNominalPowerUI(house);
-                    uS172.run();
+                    EditDevicesUI devicesUI = new EditDevicesUI(house);
+                    devicesUI.deactivateDevice();
                     break;
                 case 3:
                     GetRoomTotalNominalPowerUI ui230 = new GetRoomTotalNominalPowerUI(house);
                     ui230.showTotalNominalPowerRoom();
                     break;
                 case 4:
-                    System.out.println("US 752 GetEnergyConsumptionOfWaterHeatingUI is under maintenance, it will be available shortly");
+                    GetGridTotalNominalPowerUI uS172 = new GetGridTotalNominalPowerUI(house);
+                    uS172.run();
                     break;
                 case 5:
                     System.out.println("US 705 DisplayCustomTotalNominalPower is under maintenance, it will be available shortly");
+                    DisplayVariableTotalNominalPowerUI nominalPowerUI = new DisplayVariableTotalNominalPowerUI(house);
+                    nominalPowerUI.run();
                     break;
                 case 6:
-                    EditDevicesUI devicesUI = new EditDevicesUI(house);
-                    devicesUI.deactivateDevice();
-                    break;
-                case 7:
                     GetEnergyConsumptionInPeriodUI uiEC = new GetEnergyConsumptionInPeriodUI(house);
                     uiEC.selectOption();
                     break;
-                case 8:
+                case 7:
                     System.out.println("US730 EnergyConsumptionDataSeries is under maintenance, it will be available shortly");
                     System.out.println(house.getRoomList().getRoomList().get(2).getDeviceList().get(1).getActivityLogSum());
+                    break;
+                case 8:
+                    System.out.println("US 752 GetEnergyConsumptionOfWaterHeatingUI is under maintenance, it will be available shortly");
                     break;
                 default:
                     //no action needed
