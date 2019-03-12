5831a042edeaa04f3df3883f01ba77a0f3d8b40a
amilcarGomes <1181686@isep.ipp.pt>
2019-03-12 16:08:56 +0000

    Create Fan (device, device type, device specs and tests). Close #93.

--- src/main/java/smarthome/model/WashingMachineSpecs.java
+++ src/main/java/smarthome/model/WashingMachineSpecs.java
@@ -43,7 +43,7 @@ public class WashingMachineSpecs implements DeviceSpecs {
 
     /**
      * Get device type
-     * @return Devcie type
+     * @return Device type
      */
     @Override
     public String getDeviceType() {
