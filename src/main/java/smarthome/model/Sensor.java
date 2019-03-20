f459cd37fc2b16bdea03830efdaae0f33c35471e
amilcarGomes <1181686@isep.ipp.pt>
2019-03-20 15:41:18 +0000

    Update deactivate and reactivate methods (documentation). Ref #119

--- src/main/java/smarthome/model/Sensor.java
+++ src/main/java/smarthome/model/Sensor.java
@@ -17,44 +17,49 @@ public class Sensor {
     private SensorType sensorType;
     private Calendar startDate;
     private String unit;
+    private boolean active;
     private ReadingList readingList;
 
     /**
      * Constructor used to create internal sensors which, unlike external sensors, don't require location coordinates.
+     *
      * @param designation String parameter to specify sensor's designation
-     * @param startDate specifies the sensor start date as a Calendar dataType
-     * @param sensorType specifies the sensor type as a SensorType instance
-     * @param unit String parameter to specify sensor's unit of measure
-     * @param readings specifies the sensor's readingList
+     * @param startDate   specifies the sensor start date as a Calendar dataType
+     * @param sensorType  specifies the sensor type as a SensorType instance
+     * @param unit        String parameter to specify sensor's unit of measure
+     * @param readings    specifies the sensor's readingList
      */
     public Sensor(String designation, Calendar startDate, SensorType sensorType, String unit, ReadingList readings) {
-        if (nameIsValid(designation)) {
+        if (nameIsValid (designation)) {
             this.designation = designation;
             this.startDate = startDate;
             this.sensorType = sensorType;
             this.unit = unit;
+            this.active = true;
             this.readingList = readings;
         }
     }
 
     /**
      * Constructor used to create external sensors which require location coordinates.
-     * @param id String parameter to specify sensor's id
+     *
+     * @param id          String parameter to specify sensor's id
      * @param designation String parameter to specify sensor's designation
-     * @param startDate  specifies the sensor start date as a Calendar dataType
+     * @param startDate   specifies the sensor start date as a Calendar dataType
      * @param geoLocation specifies the sensor GPS coordinates
-     * @param sensorType specifies the sensor start date as a Calendar variable
-     * @param unit String parameter to specify sensor's unit of measure
-     * @param readings specifies the sensor's readingList
+     * @param sensorType  specifies the sensor start date as a Calendar variable
+     * @param unit        String parameter to specify sensor's unit of measure
+     * @param readings    specifies the sensor's readingList
      */
     public Sensor(String id, String designation, Calendar startDate, Location geoLocation, SensorType sensorType, String unit, ReadingList readings) {
-        if (nameIsValid(designation)) {
-            this.id=id;
+        if (nameIsValid (designation)) {
+            this.id = id;
             this.designation = designation;
             this.startDate = startDate;
             this.location = geoLocation;
             this.sensorType = sensorType;
             this.unit = unit;
+            this.active = true;
             this.readingList = readings;
         }
     }
@@ -69,10 +74,10 @@ public class Sensor {
      * @return true if name sensorDesignation is valid, if it is not null or empty
      */
     public boolean nameIsValid(String name) {
-        if (name.trim().isEmpty()) {
+        if (name.trim ().isEmpty ()) {
             return false;
         }
-        return name.matches("[A-Za-z0-9 \\- ]*");
+        return name.matches ("[A-Za-z0-9 \\-]*");
     }
 
     /**
@@ -81,7 +86,7 @@ public class Sensor {
      * @param sensorDesignation sensor's name String
      */
     public boolean setSensorDesignation(String sensorDesignation) {
-        if (nameIsValid(sensorDesignation)) {
+        if (nameIsValid (sensorDesignation)) {
             this.designation = sensorDesignation;
             return true;
         }
@@ -143,7 +148,7 @@ public class Sensor {
      * @return calculated distance betwwen both objects
      */
     public double calcLinearDistanceBetweenTwoSensors(Sensor sensor1, Sensor sensor2) {
-        return Utils.round(this.location.calcLinearDistanceBetweenTwoPoints(sensor1.getLocation(), sensor2.getLocation()),2);
+        return Utils.round (this.location.calcLinearDistanceBetweenTwoPoints (sensor1.getLocation (), sensor2.getLocation ()), 2);
     }
 
 
@@ -153,12 +158,12 @@ public class Sensor {
      * @return the last reading of a list of readings
      */
     public Reading getLastReadingPerSensor() {
-        return this.readingList.getLastReading();
+        return this.readingList.getLastReading ();
     }
 
     public double getLastReadingValuePerSensor() {
         double lastValue;
-        lastValue = this.readingList.getReadingsList().get(this.readingList.getReadingsList().size() - 1).returnValueOfReading();
+        lastValue = this.readingList.getReadingsList ().get (this.readingList.getReadingsList ().size () - 1).returnValueOfReading ();
         return lastValue;
     }
 
@@ -172,12 +177,12 @@ public class Sensor {
             return false;
         }
         Sensor sensor = (Sensor) o;
-        return Objects.equals(this.designation, sensor.designation);
+        return Objects.equals (this.designation, sensor.designation);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(this.designation, this.location, this.sensorType);
+        return Objects.hash (this.designation, this.location, this.sensorType);
     }
 
     public Calendar getStartDate() {
@@ -187,13 +192,44 @@ public class Sensor {
     public String getUnit() {
         return this.unit;
     }
-    
+
     public SensorDTO toDTO() {
-        List<ReadingDTO> readingListDTO= new ArrayList<>();
-        for (Reading reading : this.readingList.getReadingsList()) {
-            ReadingDTO readingDTO=reading.toDTO();
-            readingListDTO.add(readingDTO);
+        List<ReadingDTO> readingListDTO = new ArrayList<> ();
+        for (Reading reading : this.readingList.getReadingsList ()) {
+            ReadingDTO readingDTO = reading.toDTO ();
+            readingListDTO.add (readingDTO);
         }
-        return new SensorDTO(this.id, this.designation, readingListDTO);
+        return new SensorDTO (this.id, this.designation, readingListDTO);
     }
+
+    /**
+     * Deactivate sensor if active
+     * @return True if deactivated
+     */
+    public boolean deactivate() {
+        if (!this.active)
+            return false;
+        this.active = false;
+        return true;
+    }
+
+    /**
+     * Reactivate sensor if not active
+     * @return True if reactivated
+     */
+    public boolean reactivate(){
+        if (this.active)
+            return false;
+        this.active = true;
+        return true;
+    }
+
+    /**
+     * Check if sensor is active
+     * @return True if active. False if not active
+     */
+    public boolean isActive() {
+        return this.active;
+    }
+
 }
