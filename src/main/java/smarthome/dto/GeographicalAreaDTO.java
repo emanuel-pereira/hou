e4540060a8fce737bc7ac52ada37990761382901
Patricia Castro <1181688@isep.ipp.pt>
2019-03-23 17:59:07 +0000

    Ref #121 - Added LocationDTO, OccupationAreaDTO and SensorTypeDTO classes; Added constructor and fromDTO method to GeographicalAreaDTO; Added constructor and fromDTO method to SensorDTO

--- src/main/java/smarthome/dto/GeographicalAreaDTO.java
+++ src/main/java/smarthome/dto/GeographicalAreaDTO.java
@@ -1,19 +1,36 @@
 package smarthome.dto;
 
 
+import smarthome.model.*;
+
 import java.util.List;
 
 public class GeographicalAreaDTO {
 
     private String identification;
     private String designation;
-    private List<SensorDTO> sensorList;
+    private String typeOfGa;
+    private LocationDTO locationDTO;
+    private List<SensorDTO> sensorListDTO;
+    private OccupationAreaDTO occupationAreaDTO;
+
 
     public GeographicalAreaDTO(String identification, String designation, List<SensorDTO> sensorList) {
         this.identification = identification;
         this.designation = designation;
-        this.sensorList=sensorList;
+        this.sensorListDTO = sensorList;
     }
+
+    public GeographicalAreaDTO(String identification, String designation, String typeOfGa, LocationDTO location, List<SensorDTO> sensorList, OccupationAreaDTO occupationArea) {
+        this.identification = identification;
+        this.designation = designation;
+        this.typeOfGa = typeOfGa;
+        this.locationDTO = location;
+        this.sensorListDTO = sensorList;
+        this.occupationAreaDTO = occupationArea;
+
+    }
+
 
     public String getIdentification() {
         return identification;
@@ -24,6 +41,22 @@ public class GeographicalAreaDTO {
     }
 
     public List<SensorDTO> getSensorListDTO() {
-        return sensorList;
+        return sensorListDTO;
     }
+
+    public GeographicalArea fromDTO() {
+
+        SensorList sensorList = new SensorList();
+        OccupationArea occupation = this.occupationAreaDTO.fromDTO();
+        Location location = this.locationDTO.fromDTO();
+
+        for (SensorDTO sensorDTO : this.sensorListDTO) {
+            Sensor sensor = sensorDTO.fromDTO();
+            sensorList.addSensor(sensor);
+        }
+        return new GeographicalArea(this.identification, this.designation, this.typeOfGa, occupation, location);
+    }
+
 }
+
+
--- src/main/java/smarthome/dto/SensorDTO.java
+++ src/main/java/smarthome/dto/SensorDTO.java
@@ -1,33 +1,64 @@
 package smarthome.dto;
 
+import smarthome.model.*;
+
+
+import java.util.Calendar;
+
 import java.util.List;
 
 public class SensorDTO {
 
     private String id;
     private String designation;
-    private List <ReadingDTO> readingListDTO;
+    private Calendar startDate;
+    private LocationDTO locationDTO;
+    private SensorTypeDTO sensorTypeDTO;
+    private String unit;
+    private List<ReadingDTO> readingListDTO;
+
 
     public SensorDTO(String id, String designation, List<ReadingDTO> readingListDTO) {
         this.id = id;
         this.designation = designation;
-        this.readingListDTO=readingListDTO;
+        this.readingListDTO = readingListDTO;
     }
+
     public SensorDTO(String id, String designation) {
         this.id = id;
         this.designation = designation;
     }
 
+    public SensorDTO(String id, String designation, Calendar startDate, LocationDTO location, SensorTypeDTO sensorType, String unit, List<ReadingDTO> readingList) {
+        this.id = id;
+        this.designation = designation;
+        this.startDate = startDate;
+        this.locationDTO = location;
+        this.sensorTypeDTO = sensorType;
+        this.unit = unit;
+        this.readingListDTO = readingList;
+
+    }
+
+
     public String getId() {
-        return id;
+        return this.id;
     }
 
     public String getDesignation() {
-        return designation;
+        return this.designation;
     }
 
-    public List<ReadingDTO> getReadingListDTO(){
-        return readingListDTO;
+    public List<ReadingDTO> getReadingList() {
+        return this.readingListDTO;
 
+    }
+
+    public Sensor fromDTO() {
+        SensorType sensorType = this.sensorTypeDTO.fromDTO();
+        Location location = this.locationDTO.fromDTO();
+        ReadingList readingList = new ReadingList();
+
+        return new Sensor(this.id, this.designation, this.startDate, location, sensorType, this.unit, readingList);
     }
 }
