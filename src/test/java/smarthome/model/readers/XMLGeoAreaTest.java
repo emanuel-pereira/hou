3e02d633c468f6e0f658f0ffec457d1aabe34fdb
Patricia Castro <1181688@isep.ipp.pt>
2019-03-27 18:52:14 +0000

    Ref #121 - Added tests to XMLGeoAreaTest class

--- src/test/java/smarthome/model/readers/XMLGeoAreaTest.java
+++ src/test/java/smarthome/model/readers/XMLGeoAreaTest.java
@@ -2,13 +2,11 @@ package smarthome.model.readers;
 
 import org.junit.jupiter.api.Test;
 import smarthome.model.GeographicalArea;
-import smarthome.model.Location;
-import smarthome.model.OccupationArea;
+import smarthome.model.Sensor;
 
-import java.io.IOException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.Arrays;
+
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -21,14 +19,14 @@ class XMLGeoAreaTest {
         Path path = Paths.get("resources/DataSet_sprint05_GA.xml");
         XMLGeoArea xmlReader = new XMLGeoArea();
 
-       int result = xmlReader.importData(path).size();
+        int result = xmlReader.importData(path).size();
 
-        assertEquals(2,result);
+        assertEquals(2, result);
 
     }
 
     @Test
-    void checkGAImport () {
+    void checkGAImport() {
         Path path = Paths.get("resources/DataSet_sprint05_GA.xml");
         XMLGeoArea xmlReader = new XMLGeoArea();
 
@@ -43,15 +41,60 @@ class XMLGeoAreaTest {
     }
 
     @Test
-    void checkOccupationAreaImport (){
+    void checkOccupationAreaImport() {
         Path path = Paths.get("resources/DataSet_sprint05_GA.xml");
         XMLGeoArea xmlReader = new XMLGeoArea();
 
-        List<GeographicalArea> gaListInFile = xmlReader.importData(path);
+        List<GeographicalArea> gaList = xmlReader.importData(path);
 
-        GeographicalArea porto = gaListInFile.get(0);
+        GeographicalArea ga = gaList.get(0);
         double expected = 0.261;
-        double result = porto.getOccupation().getWidth();
+        double result = ga.getOccupation().getWidth();
+
+        double expected2 = 0.249;
+        double result2 = ga.getOccupation().getLength();
+
+        assertEquals(expected, result);
+        assertEquals(expected2, result2);
+    }
+
+    @Test
+    void checkLocationImport() {
+        Path path = Paths.get("resources/DataSet_sprint05_GA.xml");
+        XMLGeoArea xmlReader = new XMLGeoArea();
+
+        List<GeographicalArea> gaList = xmlReader.importData(path);
+
+        GeographicalArea ga = gaList.get(1);
+
+        double expected = 41.149937;
+        double result = ga.getLocation().getLatitude();
+
+        double expected2 = -8.610857;
+        double result2 = ga.getLocation().getLongitude();
+
+        double expected3 = 118;
+        double result3 = ga.getLocation().getAltitude();
+
+
+        assertEquals(expected, result);
+        assertEquals(expected2, result2);
+        assertEquals(expected3, result3);
+    }
+
+    @Test
+    void checkGASensorsImport() {
+        Path path = Paths.get("resources/DataSet_sprint05_GA.xml");
+        XMLGeoArea xmlReader = new XMLGeoArea();
+
+        List<GeographicalArea> gaList = xmlReader.importData(path);
+
+        GeographicalArea ga = gaList.get(1);
+        List<Sensor> sensorList = ga.getSensorListInGA().getSensorList();
+
+        int expected = 2;
+        int result = sensorList.size();
+
         assertEquals(expected, result);
     }
 }
