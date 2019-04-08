77d47a7603e6e31a31a3d078105da1478aa4acbb
amilcarGomes <1181686@isep.ipp.pt>
2019-04-08 09:57:24 +0100

    Change annotation form @Component to @Repository. Ref #137

--- src/main/java/smarthome/repository/RoomRepository.java
+++ src/main/java/smarthome/repository/RoomRepository.java
@@ -1,9 +1,9 @@
 package smarthome.repository;
 
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
 import smarthome.model.Room;
 
-@Component
+@Repository
 public interface RoomRepository extends CrudRepository<Room, String> {
 }
