25d2f2ea50cd0043121bfb0230c79fd871916159
José Ferreira <1181713@isep.ipp.pt>
2019-04-24 14:41:38 +0100

    Close #96 Wine Cooler device creation implementation

---
+++ src/main/java/smarthome/model/devices/WineCooler.java
@@ -0,0 +1,26 @@
+package smarthome.model.devices;
+
+import smarthome.model.DeviceSpecs;
+
+public class WineCooler extends GenericDevice {
+
+    /**
+     * Public constructor for creating an instance of a WineCooler which inherits all characteristics of a Generic Device
+     * @param deviceName name of the device
+     * @param deviceSpecs specific attributes of the device WineCooler
+     * @param nominalPower integer value representing the nominal power of a device
+     */
+    public WineCooler(String deviceName, DeviceSpecs deviceSpecs, double nominalPower) {
+        super(deviceName, deviceSpecs, nominalPower);
+    }
+
+    /**
+     *
+     * @return the estimated daily energy consumption of a wine cooler given its annual energy consumption
+     */
+    @Override
+    public double getEstimatedEnergyConsumption(){
+        return this.getNominalPower()/365;
+    }
+
+}
