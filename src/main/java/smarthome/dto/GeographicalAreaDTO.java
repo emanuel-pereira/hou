799dd15f343962fa8adb7f02cd9045b38d74d002
Patricia Castro <1181688@isep.ipp.pt>
2019-05-21 12:14:12 +0100

    Address #221: Added GeographicalAreaDTO constructor method to include all GeographicalArea attributes and added setters and getters for all the attributes; Created method in GeographicalAreaDTO and TypeGADTO classes to transform a DTO into an object; Added setters to LocationDTO and OccupationAreaDTO classes

--- src/main/java/smarthome/dto/GeographicalAreaDTO.java
+++ src/main/java/smarthome/dto/GeographicalAreaDTO.java
@@ -1,16 +1,21 @@
 package smarthome.dto;
 
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import smarthome.model.GeographicalArea;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 
+@JsonSerialize
 public class GeographicalAreaDTO {
 
     private String identification;
     private String designation;
-    private List<SensorDTO> sensorListDTO= new ArrayList<>();
+    private TypeGADTO type;
+    private OccupationAreaDTO occupationDto;
+    private LocationDTO locationDto;
+    private List<SensorDTO> sensorListDTO = new ArrayList<>();
     private GeographicalArea parentGA;
 
     public GeographicalAreaDTO() {
@@ -19,6 +24,15 @@ public class GeographicalAreaDTO {
     public GeographicalAreaDTO(String identification, String designation, List<SensorDTO> sensorListDTO) {
         this.identification = identification;
         this.designation = designation;
+        this.sensorListDTO = sensorListDTO;
+    }
+
+    public GeographicalAreaDTO(String identification, String designation, TypeGADTO type, OccupationAreaDTO occupationDto, LocationDTO locationDto, List<SensorDTO> sensorListDTO) {
+        this.identification = identification;
+        this.designation = designation;
+        this.type = type;
+        this.occupationDto = occupationDto;
+        this.locationDto = locationDto;
         this.sensorListDTO = sensorListDTO;
     }
 
@@ -54,12 +68,42 @@ public class GeographicalAreaDTO {
         this.designation = designation;
     }
 
+    public TypeGADTO getType() {
+        return type;
+    }
+
+    public void setType(TypeGADTO type) {
+        this.type = type;
+    }
+
+    public OccupationAreaDTO getOccupation() {
+        return occupationDto;
+    }
+
+    public void setOccupation(OccupationAreaDTO occupationDto) {
+        this.occupationDto = occupationDto;
+    }
+
+    public LocationDTO getLocation() {
+        return locationDto;
+    }
+
+    public void setLocation(LocationDTO locationDto) {
+        this.locationDto = locationDto;
+    }
+
     public List<SensorDTO> getSensorListDTO() {
         return sensorListDTO;
     }
 
     public void setSensorListDTO(List<SensorDTO> sensorList) {
         this.sensorListDTO = sensorList;
+    }
+
+
+    public GeographicalArea fromDTO() {
+
+        return new GeographicalArea(this.identification, this.designation, this.type.fromDTO(), this.occupationDto.fromDTO(), this.locationDto.fromDTO());
     }
 
     @Override
