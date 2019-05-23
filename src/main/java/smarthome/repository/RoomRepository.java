688b00d284f5261fa7c42e6a2199456cabae6f9f
Filipe Doria <1130778@isep.ipp.pt>
2019-05-23 11:17:50 +0100

    Addresses #237: - Creation of query method to find all rooms attached to a house grid;

--- src/main/java/smarthome/repository/RoomRepository.java
+++ src/main/java/smarthome/repository/RoomRepository.java
@@ -2,8 +2,13 @@ package smarthome.repository;
 
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
+import smarthome.dto.GridDTO;
 import smarthome.model.Room;
+
+import java.util.List;
 
 @Repository
 public interface RoomRepository extends CrudRepository<Room, String> {
+
+    List<Room> findAllByHouseGrid(Long id);
 }
