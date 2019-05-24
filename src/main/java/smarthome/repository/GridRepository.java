d9567f3fd64bedf0f3917af2730242e4152f8812
Filipe Doria <1130778@isep.ipp.pt>
2019-05-24 11:12:13 +0100

    Addresses #238, added missing method from last commit which queries the grids database for an house grid with a specific designation; -

--- src/main/java/smarthome/repository/GridRepository.java
+++ src/main/java/smarthome/repository/GridRepository.java
@@ -6,4 +6,11 @@ import smarthome.model.HouseGrid;
 
 @Repository
 public interface GridRepository extends CrudRepository<HouseGrid, Long> {
+
+    /**
+     * This method retrieves a house grid based on its name
+     * @param name designation to compare
+     * @return housegrid object that matches the queried designation
+     */
+    HouseGrid findHouseGridByDesignation(String name);
 }
