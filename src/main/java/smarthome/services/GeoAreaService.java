99366db007b0b8b6079cefe3d9504bde30379652
Patricia Castro <1181688@isep.ipp.pt>
2019-06-14 12:51:52 +0100

    Address #278: Corrected setParentGaWebCTRL() of GeoAreaService to use the findById() method of the GeoRepository; Added addNewGeoArea(), geoAreaIsValid() and findById() methods to GeoAreaService class; Corrected findAll() method of GeoAreaService class

--- src/main/java/smarthome/services/GeoAreaService.java
+++ src/main/java/smarthome/services/GeoAreaService.java
@@ -1,31 +1,33 @@
 package smarthome.services;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 import smarthome.dto.GeographicalAreaDTO;
-import smarthome.mapper.GeographicalAreaMapper;
+import smarthome.dto.TypeGADTO;
 import smarthome.model.GeographicalArea;
 import smarthome.repository.Repositories;
 
+import java.security.InvalidParameterException;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
+import java.util.Optional;
 
 
 @Service
 public class GeoAreaService {
 
 
-    private GeographicalAreaMapper geoMapper;
-
+    private ModelMapper mapper;
 
     /**
      * Constructor initialize GeoAreaService
      */
 
-    @Autowired
     public GeoAreaService() {
-        this.geoMapper = new GeographicalAreaMapper();
+
+        this.mapper = new ModelMapper();
+
+
     }
 
 
@@ -37,14 +39,15 @@ public class GeoAreaService {
      * @return true or false.
      */
 
+
     public boolean setParentGaWebCTRL(String id, String idParent) {
 
         if (!checkIfIdExists(id)) {
             return false;
         }
 
-        GeographicalArea GA1 = findByIdGa(id);
-        GeographicalArea GA2 = findByIdGa(idParent);
+        GeographicalArea GA1 = Repositories.getGeoRepository().findById(id).get();
+        GeographicalArea GA2 = Repositories.getGeoRepository().findById(idParent).get();
 
         if (!GA1.equals(GA2))
             GA1.setParentGa(GA2);
@@ -53,14 +56,63 @@ public class GeoAreaService {
         return true;
     }
 
-    public void saveGA(GeographicalArea ga){
+    /*   public boolean setParentGaWebCTRL(String id, String idParent) {
 
-        Repositories.getGeoRepository().save(ga);
+        if (!checkIfIdExists(id)) {
+            return false
+        }
+
+        GeographicalArea GA1 = findByIdGa(id)
+        GeographicalArea GA2 = findByIdGa(idParent)
+
+        if (!GA1.equals(GA2))
+            GA1.setParentGa(GA2)
+
+        Repositories.getGeoRepository().save(GA1)
+        return true
+    }*/
+
+
+    public GeographicalAreaDTO addNewGeoArea(GeographicalAreaDTO geoAreaDTO) throws InvalidParameterException {
+
+
+        TypeGADTO typeDto = geoAreaDTO.getType();
+
+        if (!Repositories.getTypeGARepository().existsByType(typeDto.getType())) {
+            throw new InvalidParameterException();
+        }
+
+        GeographicalArea newGeoArea = mapper.map(geoAreaDTO, GeographicalArea.class);
+        newGeoArea.setType(Repositories.getTypeGARepository().findByType(typeDto.getType()));
+
+        if (!geoAreaIsValid(newGeoArea)) {
+            throw new InvalidParameterException();
+        }
+        Repositories.getGeoRepository().save(newGeoArea);
+        return geoAreaDTO;
+
     }
 
-    public GeographicalArea findByIdGa(String id) {
-        return Repositories.getGeoRepository().findById(id).get();
+
+    private boolean geoAreaIsValid(GeographicalArea geoArea) {
+
+        return (geoArea.getIdentification() != null &&
+                geoArea.getDesignation() != null &&
+                geoArea.getType() != null &&
+                geoArea.getLocation() != null &&
+                geoArea.getOccupation() != null);
+
     }
+
+
+    public GeographicalAreaDTO findById(String id) throws NoSuchFieldException {
+        Optional<GeographicalArea> optional = Repositories.getGeoRepository().findById(id);
+        if (!optional.isPresent())
+            throw new NoSuchFieldException();
+        GeographicalArea temp = optional.get();
+        return this.mapper.map(temp, GeographicalAreaDTO.class);
+    }
+
 
     public boolean checkIfIdExists(String id) {
 
@@ -72,13 +124,12 @@ public class GeoAreaService {
     }
 
     public List<GeographicalAreaDTO> findAll() {
-
-        Iterable<GeographicalArea> GAs = Repositories.getGeoRepository().findAll();
-        List<GeographicalArea> GAList = new ArrayList<>();
-        for (GeographicalArea geographicalArea : GAs) {
-            GAList.add(geographicalArea);
-        }
-        return Collections.unmodifiableList(geoMapper.toDtoListParent(GAList));
+        List<GeographicalAreaDTO> geoAreas = new ArrayList<>();
+        Repositories.getGeoRepository().findAll().forEach(geographicalArea -> {
+            GeographicalAreaDTO temp = this.mapper.map(geographicalArea, GeographicalAreaDTO.class);
+            geoAreas.add(temp);
+        });
+        return geoAreas;
     }
 
 
