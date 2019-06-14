29a34e09acd62a28c6b4084fae0c463ba440d260
amilcarGomes <1181686@isep.ipp.pt>
2019-06-14 11:15:07 +0100

    Add tests with the new Mockito structure to: findAllRooms, createRoom, findOneRoomExists, findOneRoomDontExist and editRoom. Add the parameter RoomService to the constructor. Ref #281

--- src/test/java/smarthome/controller/rest/RoomCTRLTest.java
+++ src/test/java/smarthome/controller/rest/RoomCTRLTest.java
@@ -1,72 +1,130 @@
-//FIXME
-/*package smarthome.controller.rest;
+package smarthome.controller.rest;
 
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import smarthome.dto.RoomDetailDTO;
+import smarthome.model.Room;
+import smarthome.repository.HouseGridRepository;
+import smarthome.repository.RoomRepository;
+import smarthome.services.RoomService;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@ExtendWith(SpringExtension.class)
-@SpringBootTest
 @AutoConfigureMockMvc
+class RoomCTRLTest {
 
-public class RoomCTRLTest {
+    @Mock
+    private RoomRepository roomRepository;
 
-    @Autowired
+    @Mock
+    private HouseGridRepository gridRepository;
+
+    private RoomService roomService;
+
+    private RoomCTRL roomCtrl;
+
     private MockMvc mockMvc;
+
+    @BeforeEach
+    void setup() {
+        MockitoAnnotations.initMocks(this);
+        this.roomService = new RoomService(this.roomRepository, this.gridRepository);
+        this.roomCtrl = new RoomCTRL(roomService);
+        this.mockMvc = MockMvcBuilders.standaloneSetup(roomCtrl).build();
+    }
 
     @Test
     @DisplayName("Get request with success")
-    void findAll() throws Exception {
-        this.mockMvc.perform(get("/rooms"))
+    void findAllRooms() throws Exception {
+        Room room1 = new Room("B107", "Classroom", 1, 2, 3, 1.5);
+        Room room2 = new Room("B208", "Classroom", 2, 2.5, 3, 1.5);
+        List<Room> list = new ArrayList<>();
+        list.add(room1);
+        list.add(room2);
+        ObjectMapper mapper = new ObjectMapper();
+        String jsonInString = mapper.writeValueAsString(list);
+
+        when(roomRepository.findAll()).thenReturn(Stream.of(room1, room2).collect(Collectors.toList()));
+
+        this.mockMvc.perform(get("/rooms")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(jsonInString))
                 .andExpect(status().isOk());
     }
 
     @Test
-    @DisplayName("Create a room with success and can´t create another one with the same Id")
-    void createRoomWithoutSuccess() throws Exception {
-        RoomDetailDTO room = new RoomDetailDTO("B018","Classroom",0,3.0,3.5,4.0);
-        mockMvc.perform(post("/rooms")
-                .contentType(MediaType.APPLICATION_JSON)
-                .content(asJsonString(room)))
-                .andExpect(status().isCreated());
+    @DisplayName("Create a room with success")
+    void createRoom() throws Exception {
+        Room room = new Room("B018", "Classroom", 1, 3.0, 2.5, 2.0);
+        RoomDetailDTO roomDto = new RoomDetailDTO("B018", "Classroom", 1, 3.0, 2.5, 2.0);
+        ObjectMapper mapper = new ObjectMapper();
+        String jsonInString = mapper.writeValueAsString(roomDto);
 
-        RoomDetailDTO room1 = new RoomDetailDTO("B018","Classroom",0,3.0,3.5,4.0);
-        mockMvc.perform(post("/rooms")
+        when(roomRepository.save(room)).thenReturn(room);
+        when(roomRepository.findById("B018")).thenReturn(Optional.of(room));
+
+        this.mockMvc.perform(post("/rooms")
                 .contentType(MediaType.APPLICATION_JSON)
-                .content(asJsonString(room1)))
-                .andExpect(status().isUnauthorized());
+                .content(jsonInString))
+                .andExpect(status().isCreated());
     }
 
     @Test
-    @DisplayName("Create a different room with success")
-    void createAnotherRoomWithSuccess() throws Exception {
-        RoomDetailDTO room = new RoomDetailDTO("B120", "Meeting room", 1, 4.0, 4.5, 4.0);
-        mockMvc.perform(post("/rooms")
-                .contentType(MediaType.APPLICATION_JSON)
-                .content(asJsonString(room)))
-                .andExpect(status().isCreated());
+    @DisplayName("Try to find a room that with success")
+    void findOneRoomExists() throws Exception {
+        Room room = new Room("B310", "Classroom",3,2.5,2.5,2);
+        when(roomRepository.existsById("B310")).thenReturn(true);
+        when(roomRepository.findById("B310")).thenReturn(Optional.of(room));
+        this.mockMvc.perform(get("/rooms/B310")).andExpect(status().isOk());
     }
 
-    private static String asJsonString(final Object obj) {
-        try {
-            return new ObjectMapper().writeValueAsString(obj);
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
+    @Test
+    @DisplayName("Try to find a room that doesn't exist and get not found")
+    void findOneRoomDontExist() throws Exception {
+        Room room = new Room("B310", "Classroom",3,2.5,2.5,2);
+        when(roomRepository.existsById("B310")).thenReturn(true);
+        when(roomRepository.findById("B310")).thenReturn(Optional.of(room));
+        this.mockMvc.perform(get("/rooms/B31")).andExpect(status().isNotFound());
     }
+
+
+    @Test
+    @DisplayName("Edit a room with success")
+    void editRoom() throws Exception {
+        Room room = new Room("B018", "Classroom", 1, 3.0, 2.5, 2.0);
+        RoomDetailDTO roomDto = new RoomDetailDTO("B018", "Classroom", 1, 3.0, 2.5, 2.0);
+        ObjectMapper mapper = new ObjectMapper();
+        String jsonInString = mapper.writeValueAsString(roomDto);
+
+        when(roomRepository.existsById("B018")).thenReturn(true);
+        when(roomRepository.save(room)).thenReturn(room);
+        when(roomRepository.findById("B018")).thenReturn(Optional.of(room));
+
+        this.mockMvc.perform(put("/rooms/B018")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(jsonInString))
+                .andExpect(status().isOk());
+    }
+
 
 }
-*/
