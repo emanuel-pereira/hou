c0696cc7f4eba3d31dddd855ec2896f1a4958362
amilcarGomes <1181686@isep.ipp.pt>
2019-06-17 16:51:56 +0100

    Add documentation. Ref #294

--- src/main/java/smarthome/controller/cli/ListRoomsOfHouseCTRL.java
+++ src/main/java/smarthome/controller/cli/ListRoomsOfHouseCTRL.java
@@ -7,28 +7,47 @@ import smarthome.services.RoomService;
 
 import java.util.List;
 
-import static smarthome.model.House.getHouseRoomList;
-
 public class ListRoomsOfHouseCTRL {
 
     private final RoomService roomService;
 
+
+    /**
+     * ListRoomsOfHouseCTRL constructor
+     */
     public ListRoomsOfHouseCTRL() {
         this.roomService = new RoomService();
     }
 
+    /**
+     * Test constructor
+     * @param rRepository RoomRepository
+     * @param gRepository HouseGridRepository
+     */
     public  ListRoomsOfHouseCTRL(RoomRepository rRepository, HouseGridRepository gRepository) {
         this.roomService =new RoomService(rRepository,gRepository);
     }
 
+    /**
+     * Check the number of rooms saved in the repository
+     * @return Number of rooms
+     */
     public long roomListSize(){
         return this.roomService.size();
     }
 
+    /**
+     * Check if the repository is empty
+     * @return True if empty
+     */
     public boolean roomListEmpty(){
         return this.roomService.checkIfRoomRepositoryEmpty();
     }
 
+    /**
+     * Find all the rooms in the repository
+     * @return List of RoomDTOs
+     */
     public List<RoomDTO> findAll() {
         return this.roomService.findAll();
     }
