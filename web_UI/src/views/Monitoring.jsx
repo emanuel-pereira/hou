af792d62ea05e49e46c49f64b69864df86e40c75
Francisca_1181706 <1181706@isep.ipp.pt>
2019-06-19 16:28:44 +0100

    Revert "Ref#302_New Components: HouseMonitoring + AreaMonitoring + AreaCurrentTemp| New Actions: actionsArea | New Reducers: areaMonitoringReducer_Edit: Monitoring + configureStore"
    This reverts commit f96f7b39

--- web_UI/src/views/Monitoring.jsx
+++ web_UI/src/views/Monitoring.jsx
@@ -1,12 +1,10 @@
 import React from "react";
-import HouseMonitoring from "../components/project/HouseMonitoring";
 
 class Monitoring extends React.Component {
   render() {
     return (
       <>
         <div className="content">
-            <HouseMonitoring/>
         </div>
       </>
     );
