d0b957ab49933a58d9f5c8488952ff680c29dd43
amilcarGomes <1181686@isep.ipp.pt>
2019-06-19 16:14:03 +0100

    Change return to Resources. Add new tests Ref #301

--- src/main/java/smarthome/controller/rest/RoomCTRL.java
+++ src/main/java/smarthome/controller/rest/RoomCTRL.java
@@ -15,7 +15,7 @@ import java.util.List;
 import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
 import static org.springframework.hateoas.mvc.ControllerLinkBuilder.methodOn;
 
-@CrossOrigin(origins = {"http://localhost:3000", "http://localhost:3001", "http://localhost:3002","http://localhost:8080", "http://localhost:8082", "https://localhost:8443"}, maxAge = 3600)
+@CrossOrigin(origins = {"http://localhost:3000", "http://localhost:3001", "http://localhost:3002", "http://localhost:8080", "http://localhost:8082", "https://localhost:8443"}, maxAge = 3600)
 @RestController
 public class RoomCTRL {
 
@@ -28,57 +28,63 @@ public class RoomCTRL {
     /**
      * Receives HTTP Get request and shows all rooms
      *
-     * @return ResponseEntity that represents the whole HTTP response with a List<RoomDTO> (the basic information of the Room)
+     * @return ResponseEntity that represents the whole HTTP response with a Resource (the basic information of the Room) with links and HttpStatus
+     * @throws NoSuchFieldException Signals that the class doesn't have a field of a specified name (because of the Optional<> return of the findById(id) method.
      */
     @GetMapping("/rooms")
-    public ResponseEntity<Object> findAll() {
-        return new ResponseEntity<>(this.roomService.findAll(), HttpStatus.OK);
-    }
-
-    /*@GetMapping("/rooms")
-    public ResponseEntity<Resources<Resource<RoomDTO>>> alternative() throws NoSuchFieldException {
+    public ResponseEntity<Resources<Resource<RoomDTO>>> findAll() throws NoSuchFieldException {
         List<Resource<RoomDTO>> rooms = new ArrayList<>();
         for (RoomDTO room : this.roomService.findAll()) {
             Resource<RoomDTO> roomDTOResource = new Resource<>(room,
-                    linkTo(methodOn(RoomCTRL.class).findOne(room.getID())).withSelfRel(),
-                    linkTo(methodOn(RoomCTRL.class).findAll()).withRel("rooms"));
+                    linkTo(methodOn(RoomCTRL.class).findOne(room.getID())).withSelfRel());
             rooms.add(roomDTOResource);
         }
-        Resources<Resource<RoomDTO>> resource = new Resources <>(rooms,
+        Resources<Resource<RoomDTO>> resource = new Resources<>(rooms,
                 linkTo(methodOn(RoomCTRL.class).findAll()).withSelfRel());
-
         return new ResponseEntity<>(resource, HttpStatus.OK);
-    }*/
+    }
 
     /**
-     * Receives HTTP Post request and creates and saves a room in the repository
+     * Receives HTTP Post request and creates and saves that room in the repository
      *
      * @param room RoomDetailDTO with the elements necessary to create a Room
-     * @return ResponseEntity that represents the whole HTTP response
+     * @return ResponseEntity that represents the whole HTTP response with a Resource (attributes of the Room) with the link to that resource and HttpStatus
+     * @throws NoSuchFieldException Signals that the class doesn't have a field of a specified name (because of the Optional<> return of the findById(id) method.
      */
     @PostMapping("/rooms")
-    public ResponseEntity<Object> createRoom(@RequestBody RoomDetailDTO room) throws NoSuchFieldException {
+    public ResponseEntity<Resource<Object>> createRoom(@RequestBody RoomDetailDTO room) throws NoSuchFieldException {
+        Resource<Object> resource;
         if (this.roomService.checkIfIDExists(room.getId())) {
-            return new ResponseEntity<>("There is already a room with this id.", HttpStatus.CONFLICT);
+            resource = new Resource<>("There is already a room with this id.");
+            return new ResponseEntity<>(resource, HttpStatus.CONFLICT);
         }
         if (this.roomService.save(room)) {
-            return new ResponseEntity<>(this.roomService.findById(room.getId()), HttpStatus.CREATED);
-        } else return new ResponseEntity<>("Could not create the room", HttpStatus.BAD_REQUEST);
+            resource = new Resource<>(room,
+                    linkTo(methodOn(RoomCTRL.class).findOne(room.getId())).withSelfRel());
+            return new ResponseEntity<>(resource, HttpStatus.CREATED);
+        } else
+            resource = new Resource<>("Could not create the room");
+        return new ResponseEntity<>(resource, HttpStatus.BAD_REQUEST);
     }
 
     /**
      * Receives HTTP Get request and shows the specific information of a room
      *
      * @param id The Id of the Room
-     * @return ResponseEntity that represents the whole HTTP response with a RoomDetailDTO (more information of the Room) if exists
+     * @return ResponseEntity that represents the whole HTTP response with a Resource (attributes of the Room) with the link to that resource and HttpStatus
+     * @throws NoSuchFieldException Signals that the class doesn't have a field of a specified name (because of the Optional<> return of the findById(id) method.
      */
     @GetMapping("/rooms/{id}")
-    public ResponseEntity<Object> findOne(@PathVariable String id) throws NoSuchFieldException {
+    public ResponseEntity<Resource<Object>> findOne(@PathVariable String id) throws NoSuchFieldException {
+        Resource<Object> resource;
         if (roomService.checkIfIDExists(id)) {
             RoomDetailDTO room = roomService.findById(id);
-            return new ResponseEntity<>(room, HttpStatus.OK);
+            resource = new Resource<>(room,
+                    linkTo(methodOn(RoomCTRL.class).findOne(room.getId())).withSelfRel());
+            return new ResponseEntity<>(resource, HttpStatus.OK);
         }
-        return new ResponseEntity<>("Room not found.", HttpStatus.NOT_FOUND);
+        resource = new Resource<>("Room not found.");
+        return new ResponseEntity<>(resource, HttpStatus.NOT_FOUND);
     }
 
     /**
@@ -90,15 +96,21 @@ public class RoomCTRL {
      * @throws NoSuchFieldException Signals that the class doesn't have a field of a specified name (because of the Optional<> return of the findById(id) method.
      */
     @PutMapping("/rooms/{id}")
-    public ResponseEntity<Object> editRoom(@PathVariable String id, @RequestBody RoomDetailDTO room) throws NoSuchFieldException {
+    public ResponseEntity<Resource<Object>> editRoom(@PathVariable String id, @RequestBody RoomDetailDTO room) throws NoSuchFieldException {
+        Resource<Object> resource;
         if (this.roomService.checkIfIDExists(id)) {
             if (this.roomService.editRoom(id, room)) {
                 RoomDetailDTO editRoom = this.roomService.findById(id);
-                return new ResponseEntity<>(editRoom, HttpStatus.OK);
+                resource = new Resource<>(editRoom,
+                        linkTo(methodOn(RoomCTRL.class).findOne(room.getId())).withSelfRel(),
+                        linkTo(methodOn(RoomCTRL.class).findAll()).withRel("rooms"));
+                return new ResponseEntity<>(resource, HttpStatus.OK);
             }
-            return new ResponseEntity<>("Could not update the room", HttpStatus.BAD_REQUEST);
+            resource = new Resource<>("Could not update the room.");
+            return new ResponseEntity<>(resource, HttpStatus.BAD_REQUEST);
         }
-        return new ResponseEntity<>("Room not found.", HttpStatus.NOT_FOUND);
+        resource = new Resource<>("Room not found.");
+        return new ResponseEntity<>(resource, HttpStatus.NOT_FOUND);
     }
 
 }
