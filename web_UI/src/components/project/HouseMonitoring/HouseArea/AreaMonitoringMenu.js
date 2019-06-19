420f7be10a83859377ae66dbfd615375a5a65ec5
Francisca_1181706 <1181706@isep.ipp.pt>
2019-06-19 17:03:15 +0100

    Ref#302_New Components: HouseMonitoring + AreaMonitoring + AreaCurrentTemp| New Actions: actionsArea | New Reducers: areaMonitoringReducer_Edit: Monitoring + configureStore

---
+++ web_UI/src/components/project/HouseMonitoring/HouseArea/AreaMonitoringMenu.js
@@ -0,0 +1,99 @@
+import React from 'react';
+import AreaCurrentTemp from "./AreaCurrentTemp";
+import AreaLastLowTemp from "./AreaLastLowTemp";
+import AreaFirstHighTemp from "./AreaFirstHighTemp";
+import AreaHighAmpliTemp from "./AreaHighAmpliTemp";
+
+class AreaMonitoring extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            showCurrent: false,
+            showLastLow: false,
+            showFirstHigh: false,
+            showHighAmpli: false,
+        }
+    }
+
+    clickToShowCurrent() {
+        this.setState({
+            showCurrent: !this.state.showCurrent,
+            showLastLow: false,
+            showFirstHigh: false,
+            showHighAmpli: false,
+        })
+    }
+
+    clickToShowLastLow() {
+        this.setState({
+            showCurrent: false,
+            showLastLow: !this.state.showLastLow,
+            showFirstHigh: false,
+            showHighAmpli: false,
+        })
+    }
+
+    clickToShowFirstHigh() {
+        this.setState({
+            showCurrent: false,
+            showLastLow: false,
+            showFirstHigh: !this.state.showFirstHigh,
+            showHighAmpli: false,
+        })
+    }
+
+    clickToShowHighAmpli() {
+        this.setState({
+            showCurrent: false,
+            showLastLow: false,
+            showFirstHigh: false,
+            showHighAmpli: !this.state.showHighAmpli,
+        })
+    }
+
+    render() {
+
+        return (
+            <div>
+                <h4>Temperature</h4>
+                <table>
+                    {/*buttons to access components*/}
+                    <td>
+                        <button onClick={this.clickToShowCurrent.bind(this)}>Current</button>
+                    </td>
+                    <td>
+                        <button onClick={this.clickToShowLastLow.bind(this)}>Last Lowest Maximum</button>
+                    </td>
+                    <td>
+                        <button onClick={this.clickToShowFirstHigh.bind(this)}>First Highest Maximum</button>
+                    </td>
+                    <td>
+                        <button onClick={this.clickToShowHighAmpli.bind(this)}>Highest Amplitude</button>
+                    </td>
+
+                </table>
+
+                {/*components calls*/}
+                <table>
+                    <tr>
+                        {this.state.showCurrent && <AreaCurrentTemp/>}
+                    </tr>
+                    <tr>
+                        {this.state.showLastLow && <AreaLastLowTemp/>}
+                    </tr>
+                    <tr>
+                        {this.state.showFirstHigh && <AreaFirstHighTemp/>}
+                    </tr>
+                    <tr>
+                        {this.state.showHighAmpli && <AreaHighAmpliTemp/>}
+                    </tr>
+                </table>
+            </div>
+        )
+
+
+    }
+}
+
+
+export default AreaMonitoring
---
+++ web_UI/src/components/project/HouseMonitoring/HouseMonitoringMenu.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import AreaMonitoringMenu from "./HouseArea/AreaMonitoringMenu";
+
+class HouseMonitoring extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            showAreaMonitoring: false,
+            showRoomMonitoring: false
+        }
+    }
+
+    clickToShowArea() {
+        this.setState({
+            showAreaMonitoring: true,
+            showRoomMonitoring: false
+        })
+    }
+
+    clickToShowRooms() {
+        this.setState({
+            showAreaMonitoring: false,
+            showRoomMonitoring: true
+        })
+    }
+
+    render() {
+        return (
+            <div>
+                {/*<h3>Choose an option</h3>*/}
+                <table>
+                    <td>
+                        <button onClick={this.clickToShowArea.bind(this)}>House's Area Monitoring</button>
+                    </td>
+                    <td>
+                        <button onClick={this.clickToShowRooms.bind(this)}>House's Room Monitoring</button>
+
+                    </td>
+                </table>
+                <table>
+                    <tr>
+                        {this.state.showAreaMonitoring && <AreaMonitoringMenu/>}
+                    </tr>
+                    <tr>
+                        {this.state.showRoomMonitoring && <p> PLEASE INSERT ROOM MONITORING COMPONENT HERE</p>}
+                    </tr>
+                </table>
+            </div>
+        )
+    }
+}
+
+export default HouseMonitoring
