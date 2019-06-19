00d7ec947bc5b285fa18656baef27136bf19af89
José Ferreira <1181713@isep.ipp.pt>
2019-06-19 23:06:02 +0100

    Close #312 deleted unused methods on typeGAService class.

--- src/main/java/smarthome/services/GaTypesService.java
+++ src/main/java/smarthome/services/GaTypesService.java
@@ -31,41 +31,6 @@ public class GaTypesService {
             this.repository = Repositories.getTypeGARepository();
     }
 
-    public TypeGADTO findByObject(Object object) {
-        setRepositories();
-
-        TypeGA newtype = null;
-        String pathvariable = object.toString();
-        try {
-            Optional<TypeGA> type = repository.findById(Long.parseLong(pathvariable));
-            if (type.isPresent())
-                newtype = type.get();
-        } catch (Exception e) {
-            newtype = repository.findByType(pathvariable);
-        }
-        return modelMapper.map(newtype, TypeGADTO.class);
-    }
-
-
-    public List<TypeGADTO> findAll() {
-        setRepositories();
-
-        List<TypeGADTO> types = new ArrayList<>();
-        repository.findAll().forEach((TypeGA typeGA) ->
-                types.add(modelMapper.map(typeGA, TypeGADTO.class)));
-        return types;
-    }
-
-    public TypeGADTO newType(TypeGADTO newType) {
-        setRepositories();
-
-        TypeGA type = modelMapper.map(newType, TypeGA.class);
-        TypeGA saved = repository.save(type);
-        //TODO implement service that looks for equals, requests objects, and saves objects
-        //TODO implement ResponseEntity<> for multiple return messages
-        return modelMapper.map(saved, TypeGADTO.class);
-    }
-
     public boolean existsByType(String type) {
         setRepositories();
         return repository.existsByType(type);
