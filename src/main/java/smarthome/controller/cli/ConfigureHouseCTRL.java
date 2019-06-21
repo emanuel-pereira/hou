37fcad2c558e3e0ac5068c11fe4a37107b4853a1
José Ferreira <1181713@isep.ipp.pt>
2019-06-21 18:29:49 +0100

    ref #318 - Deleted unused methods in ConfigureHouseCTRL and TypeGADTO class ref #319 - Implemented unit tests for  LoginViewModelTest and UserPrincipalDetailsService

--- src/main/java/smarthome/controller/cli/ConfigureHouseCTRL.java
+++ src/main/java/smarthome/controller/cli/ConfigureHouseCTRL.java
@@ -46,14 +46,6 @@ public class ConfigureHouseCTRL {
         return result.toString();
     }
 
-    /**
-     * @return the number of elements in the geographical areas list as an integer value
-     */
-
-    public int getGAListSize() {
-        return this.gaList.size();
-    }
-
     public String getIdFromIndex (int index){
         GeographicalArea geoArea = this.gaList.get(index-1);
         return geoArea.getIdentification();
