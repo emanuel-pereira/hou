e474d99fce02ac7335ae96b9fea6424ac053fae1
Patricia Castro <1181688@isep.ipp.pt>
2019-06-25 03:14:52 +0100

    Address #331: Changed buttons and creation form of CreateNewIntSensor, CreateRoom and UpdateRoom; Changed RoomDetailList table; Changed buttons and table of RoomSensors

--- web_UI/src/components/project/RoomComponents/CreateRoom.js
+++ web_UI/src/components/project/RoomComponents/CreateRoom.js
@@ -1,10 +1,12 @@
 import React from 'react';
-import { connect } from 'react-redux';
-import { createRoom } from 'actions/actionsRoom';
+import {connect} from 'react-redux';
+import {createRoom} from 'actions/actionsRoom';
 import NotificationAlert from "react-notification-alert";
 import {
     Button,
 } from "reactstrap";
+import Card from "reactstrap/es/Card";
+import CardBody from "reactstrap/es/CardBody";
 
 var options = {};
 options = {
@@ -21,10 +23,10 @@ class CreateRoom extends React.Component {
     state = {
         id: '',
         description: '',
-        floor: 0,
-        length: 0,
-        width: 0,
-        height: 0
+        floor: '',
+        length: '',
+        width: '',
+        height: ''
     };
 
     handleInputChange = e => {
@@ -38,99 +40,112 @@ class CreateRoom extends React.Component {
         this.props.onAddRoom(this.state)
         this.refs.notify.notificationAlert(options);
         this.handleReset();
-        setTimeout(this.props.onClose(),1000)
+        setTimeout(this.props.onClose(), 1000)
     };
 
     handleReset = () => {
         this.setState({
             id: '',
             description: '',
-            floor: 0,
-            length: 0,
-            width: 0,
-            height: 0
+            floor: '',
+            length: '',
+            width: '',
+            height: ''
         });
     };
 
     render() {
         return (
-            <div className="roomCreation">
-                <NotificationAlert ref="notify" />
-                <h4>New Room</h4>
-                <form onSubmit={this.handleSubmit}>
-                    <div className="form-group">
-                        ID
-                        <input
-                            type="text"
-                            className="form-control"
-                            name="id"
-                            onChange={this.handleInputChange}
-                            value={this.state.id}
-                        />
-                    </div>
-                    <div className="form-group">
-                        Description
-                        <input
-                            type="text"
-                            className="form-control"
-                            name="description"
-                            onChange={this.handleInputChange}
-                            value={this.state.description}>
-                        </input>
-                    </div>
-                    <div className="form-group">
-                        Floor
-                        <input
-                            type="text"
-                            className="form-control"
-                            name="floor"
-                            onChange={this.handleInputChange}
-                            value={this.state.floor}>
-                        </input>
-                    </div>
-                    <div className="form-group">
-                        Length
-                        <input
-                            type="text"
-                            className="form-control"
-                            name="length"
-                            onChange={this.handleInputChange}
-                            value={this.state.length}>
-                        </input>
-                    </div>
-                    <div className="form-group">
-                        Width
-                        <input
-                            type="text"
-                            className="form-control"
-                            name="width"
-                            onChange={this.handleInputChange}
-                            value={this.state.width}>
-                        </input>
-                    </div>
-                    <div className="form-group">
-                        Height
-                        <input
-                            type="text"
-                            className="form-control"
-                            name="height"
-                            onChange={this.handleInputChange}
-                            value={this.state.height}>
-                        </input>
-                    </div>
-                    <div className="buttons">
-                        <Button
-                            className="btn btn-success"
-                            color="primary"
-                            type="submit"
-                        >
-                            SAVE
-                        </Button>
-                        <Button type="button" className="btn btn-warning" onClick={this.handleReset}>
-                            RESET
-                        </Button>
-                    </div>
-                </form>
+            <div>
+                <NotificationAlert ref="notify"/>
+                <Card>
+                    <CardBody>
+                    <h4>New Room</h4>
+                    <form onSubmit={this.handleSubmit}>
+                        <div class="form-row">
+                            <div class="form-group col-md-4">
+                                <label>ID</label>
+                                <input
+                                    placeholder="ex. LR1"
+                                    type="text"
+                                    className="form-control"
+                                    name="id"
+                                    onChange={this.handleInputChange}
+                                    value={this.state.id}
+                                />
+                            </div>
+                            <div className="form-group col-md-4">
+                                <label>Description</label>
+                                <input
+                                    placeholder="ex. Living Room"
+                                    type="text"
+                                    className="form-control"
+                                    name="description"
+                                    onChange={this.handleInputChange}
+                                    value={this.state.description}>
+                                </input>
+                            </div>
+                            <div className="form-group col-md-2">
+                                <label>Floor</label>
+                                <input
+                                    placeholder="0"
+                                    type="number"
+                                    className="form-control"
+                                    name="floor"
+                                    onChange={this.handleInputChange}
+                                    value={this.state.floor}>
+                                </input>
+                            </div>
+                        </div>
+
+                        <div className="form-row">
+                            <div className="form-group col-md-2">
+                                <label>Length</label>
+                                <input
+                                    placeholder="0 m"
+                                    type="number"
+                                    className="form-control"
+                                    name="length"
+                                    onChange={this.handleInputChange}
+                                    value={this.state.length}>
+                                </input>
+                            </div>
+                            <div className="form-group col-md-2">
+                                <label>Width</label>
+                                <input
+                                    placeholder="0 m"
+                                    type="number"
+                                    className="form-control"
+                                    name="width"
+                                    onChange={this.handleInputChange}
+                                    value={this.state.width}>
+                                </input>
+                            </div>
+                            <div className="form-group col-md-2">
+                                <label>Height</label>
+                                <input
+                                    placeholder="0 m"
+                                    type="number"
+                                    className="form-control"
+                                    name="height"
+                                    onChange={this.handleInputChange}
+                                    value={this.state.height}>
+                                </input>
+                            </div>
+                        </div>
+
+                        <div>
+                            <Button className="btn btn-success" color="primary" type="submit">
+                                SAVE
+                            </Button>
+                            <Button type="button" className="btn btn-warning" onClick={this.handleReset}>
+                                RESET
+                            </Button>
+                        </div>
+
+                    </form>
+                    </CardBody>
+                </Card>
             </div>
         );
     }
